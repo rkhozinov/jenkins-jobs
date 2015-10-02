ccdefbce251561a957801c626cb7ae67e492973d
Ruslan Khozinov <ruslan.khozinov@gmail.com>
2015-10-03 00:28:05 +0300

    new file:   servers/partner-ci/8.0/builders/systest.vcenter.custom.sh     - added run script for custom job
    new file:   servers/partner-ci/8.0/builders/systest.vcenter.prepare.sh
        - run script was separated on two parts
    
    modified:   servers/partner-ci/8.0/builders/systest.vcenter.sh
        - run script was modified to run tests properly
    
    not used files were deleted:
        deleted:    servers/partner-ci/8.0/descriptions/centos.bvt_2
        deleted:    servers/partner-ci/8.0/descriptions/testrail_reports
        deleted:    servers/partner-ci/8.0/descriptions/ubuntu.bvt_2
        deleted:    servers/partner-ci/8.0/descriptions/upgradechain_tests
    
    modified:   servers/partner-ci/8.0/vcenter.yaml
        - added some changes with env_prefix, env_name
        - added custom job

--- servers/partner-ci/8.0/builders/systest.vcenter.custom.sh
+++ servers/partner-ci/8.0/builders/systest.vcenter.custom.sh
@@ -8,6 +8,10 @@ systest_parameters=''
 [ $SHUTDOWN_AFTER ] && systest_parameters+=' --destroy'
 [ $ERASE_AFTER    ] && systest_parameters+=' --erase'
 
+if [ -z $FUEL_QA_GERRIT_COMMIT ] ; then
+    for commit in $FUEL_QA_GERRIT_COMMIT; do
+        git fetch $GIT_URL "${commit}" && git cherry-pick FETCH_HEAD
+    done
+fi
 export ENV_NAME="${ENV_PREFIX}.${ISO_VERSION}"
-echo env-name: $ENV_NAME
 /btsync/tpi_systest.sh -i $ISO_PATH -d $OPENSTACK_RELEASE -t $TEST_GROUP -n $NODES_COUNT $systest_parameters
