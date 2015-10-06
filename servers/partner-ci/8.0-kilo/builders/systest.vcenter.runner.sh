5ca78f7f2e757142e1d1bd25f7b0dd7913158a48
Ruslan Khozinov <ruslan.khozinov@gmail.com>
2015-10-07 01:49:27 +0300

    modified:   builders/systest.vcenter.runner.sh               - added support for the latest iso version from the 	modified:   vcenter.yaml               - added support for the latest iso version from the                 "download_iso" job               - refactored builder for runner jobs

--- servers/partner-ci/8.0-kilo/builders/systest.vcenter.runner.sh
+++ servers/partner-ci/8.0-kilo/builders/systest.vcenter.runner.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-[ -z $ISO_FILE ] && export ISO_FILE=${ISO_FILE_80}
-export ISO_VERSION=`cut -d'-' -f4-4 <<< $ISO_FILE`
+[ -z $ISO_FILE ] && export ISO_FILE=${ISO_FILE_80-kilo}
+#export ISO_VERSION=`cut -d'-' -f4-4 <<< $ISO_FILE`
+export ISO_VERSION=$(cat iso_version.txt)
 
 echo iso $ISO_VERSION: $ISO_FILE
 echo "ISO_FILE=${ISO_FILE}" > $properties_file
