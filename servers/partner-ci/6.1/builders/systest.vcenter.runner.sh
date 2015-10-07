b2be32b7d7f6f634bd4b2a4096780a9bdddced26
Ruslan Khozinov <ruslan.khozinov@gmail.com>
2015-10-07 03:12:24 +0300

    Unused files were deleted 	deleted:    builders/download.sh 	deleted:    builders/systest.vcenter.custom.sh 	deleted:    builders/systest.vcenter.prepare.sh 	deleted:    builders/systest.vcenter.runner.sh 	deleted:    builders/systest.vcenter.sh

--- servers/partner-ci/6.1/builders/systest.vcenter.runner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-[ -z $ISO_FILE ] && export ISO_FILE=${ISO_FILE_80}
-export ISO_VERSION=`cut -d'-' -f4-4 <<< $ISO_FILE`
-
-echo iso $ISO_VERSION: $ISO_FILE
-echo "ISO_FILE=${ISO_FILE}" > $properties_file
-echo "ISO_VERSION=${ISO_VERSION}" >> $properties_file
