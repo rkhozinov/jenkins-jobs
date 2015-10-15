2028f995326740068d9f3f6b577bdbc6d29efe4f
Ruslan Khozinov <ruslan.khozinov@gmail.com>
2015-10-16 02:10:05 +0300

    Unused build is deleted 	deleted:    builders/dvs.systest.runner.sh

--- servers/partner-ci/6.1/builders/dvs.systest.runner.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-[ -z $ISO_FILE ] && export ISO_FILE=${ISO_FILE_80-kilo}
-#export ISO_VERSION=`cut -d'-' -f4-4 <<< $ISO_FILE`
-export ISO_VERSION=$(cat iso_version.txt)
-
-echo iso $ISO_VERSION: $ISO_FILE
-echo "ISO_FILE=${ISO_FILE}" > $properties_file
-echo "ISO_VERSION=${ISO_VERSION}" >> $properties_file
