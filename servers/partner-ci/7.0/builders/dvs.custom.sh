d3ff10af11b56bdd45c7dfd7214b33d1dd07456c
Ruslan Khozinov <ruslan.khozinov@gmail.com>
2015-10-16 02:20:01 +0300

    dvs: builders are added

---
+++ servers/partner-ci/7.0/builders/dvs.custom.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -ex 
+
+export ISO_PATH="$ISO_STORAGE/$ISO_FILE"
+export ISO_VERSION=`cut -d'-' -f4-4 <<< $ISO_FILE` 
+export ENV_NAME="${ENV_PREFIX}.${ISO_VERSION}"
+source $VENV_PATH/bin/activate
+
+systest_parameters=''
+[ $USE_SNAPSHOTS  ] && systest_parameters+=' --existing'
+[ $SHUTDOWN_AFTER ] && systest_parameters+=' --destroy'
+[ $ERASE_AFTER    ] && systest_parameters+=' --erase'
+
+if [ -z $FUEL_QA_GERRIT_COMMIT ] ; then
+    for commit in $FUEL_QA_GERRIT_COMMIT; do
+        git fetch $GIT_URL "${commit}" && git cherry-pick FETCH_HEAD
+    done
+fi
+/btsync/tpi_systest.sh -i $ISO_PATH -d $OPENSTACK_RELEASE -t $TEST_GROUP -n $NODES_COUNT $systest_parameters
