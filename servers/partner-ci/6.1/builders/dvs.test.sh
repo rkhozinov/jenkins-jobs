83933614f219776b185954a9d722be33f7498227
Ruslan Khozinov <ruslan.khozinov@gmail.com>
2015-10-21 18:07:13 +0300

    6.1/dvs: Tests was turned off

--- servers/partner-ci/6.1/builders/dvs.test.sh
+++ servers/partner-ci/6.1/builders/dvs.test.sh
@@ -1,31 +1,29 @@
 #!/bin/bash -e 
 
-# activate bash xtrace for script
-[[ "${DEBUG}" == "true" ]] && set -x || set +x
-
 export ISO_PATH="${ISO_STORAGE}/${ISO_FILE}"
 export ISO_VERSION=$(cut -d'-' -f3-3 <<< $ISO_FILE) 
-export FUEL_RELEASE=$(cut -d'-' -f2-2 <<< $ISO_FILE | tr -d '.') 
-
 export ENV_NAME="${ENV_PREFIX}.${ISO_VERSION}"
-export VENV_PATH="${HOME}/${FUEL_RELEASE}-venv"
+export DVS_PLUGIN_PATH=$(ls ${WORKSPACE}/fuel-plugin-vmware-dvs*.rpm)
 
-[[ -z "${DVS_PLUGIN_PATH}" ]] && export DVS_PLUGIN_PATH=$(ls ${WORKSPACE}/fuel-plugin-vmware-dvs*.rpm) || true
+export FUEL_RELEASE=$(cut -d'-' -f2-2<<< $ISO_FILE | tr -d .) 
+export VENV_PATH="${HOME}/${FUEL_RELEASE}-venv"
 
 source $VENV_PATH/bin/activate
 
 systest_parameters=''
-[[ $USE_SNAPSHOTS == "true"  ]] && systest_parameters+=' -k' || echo new env will be created
-[[ $ERASE_AFTER   == "true"  ]] && echo the env will be erased after test || systest_parameters+=' -K'
+[[ $USE_SNAPSHOTS == "true"  ]] && systest_parameters+=' --existing' || echo new env will not be created
+[[ $SHUTDOWN_AFTER == "true" ]] && systest_parameters+=' --destroy' || echo  the env will not be powered off after test
+[[ $ERASE_AFTER  == "true"  ]] && systest_parameters+=' --erase' || echo the env will not be erased after test 
 
-echo test-group: $TEST_GROUP
-echo env-name: $ENV_NAME
 echo use-snapshots: $USE_SNAPSHOTS
-echo fuel-release: $FUEL_RELEASE
+
 echo venv-path: $VENV_PATH
 echo env-name: $ENV_NAME
 echo iso-path: $ISO_PATH   
-echo plugin-path: $DVS_PLUGIN_PATH 
-echo plugin-checksum: $(md5sum -b $DVS_PLUGIN_PATH) 
+echo plugin-path: $DVS_PLUGIN_PATH
 
-./plugin_test/utils/jenkins/system_tests.sh -t test ${systest_parameters} -i ${ISO_PATH} -j ${JOB_NAME} -o --group=${TEST_GROUP}
+echo plugin path: $DVS_PLUGIN_PATH plugin checksum: $(md5sum -b $DVS_PLUGIN_PATH) 
+
+cd plugin_test/
+/btsync/tpi_systest.sh -i $ISO_PATH -d $OPENSTACK_RELEASE -t $TEST_GROUP -n $NODES_COUNT $systest_parameters
+
