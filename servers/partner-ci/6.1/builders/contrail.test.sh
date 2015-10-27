2b7e0ae3a722480c669e9a34bc3ad44fc560a713
Ruslan Khozinov <ruslan.khozinov@gmail.com>
2015-10-27 13:29:19 +0300

    Builders are added for contrail 2.0, 2.1
    	new file:   servers/partner-ci/6.1/builders/contrail.prepare.sh
    	new file:   servers/partner-ci/6.1/builders/contrail.test.sh

--- servers/partner-ci/6.1/builders/contrail.test.sh
+++ servers/partner-ci/6.1/builders/contrail.test.sh
@@ -1,39 +1,33 @@
 #!/bin/bash -e
 
-# activate bash xtrace for script
-[[ "${DEBUG}" == "true" ]] && set -x || set +x
-
-[ -z $CONTRAIL_VERSION ] && exit 1 || echo contrail version is $CONTRAIL_VERSION
-
 export ISO_PATH="${ISO_STORAGE}/${ISO_FILE}"
-export ISO_VERSION=$(cut -d'-' -f3-3 <<< $ISO_FILE) 
-export FUEL_RELEASE=$(cut -d'-' -f2-2 <<< $ISO_FILE | tr -d '.') 
-
+export ISO_VERSION=$(cut -d'-' -f3-3 <<< $ISO_FILE)
 export ENV_NAME="${ENV_PREFIX}.${ISO_VERSION}"
-export VENV_PATH="${HOME}/${FUEL_RELEASE}-venv"
 
-[[ -z ${CONTRAIL_PLUGIN_PATH} ]] && export CONTRAIL_PLUGIN_PATH=$(ls ${WORKSPACE}/contrail*.rpm) \
-                                 || echo CONTRAIL_PLUGIN_PATH=$CONTRAIL_PLUGIN_PATH
+export CONTRAIL_PLUGIN_PATH=$(ls ${WORKSPACE}/fuel-plugin-contrail*.rpm)
+
+export JUNIPER_PKG_PATH="/storage/contrail/22/"
+export CONTRAIL_PLUGIN_PACK_CEN_PATH=$(find ${JUNIPER_PKG_PATH} -type f -name '*rpm' )
+export CONTRAIL_PLUGIN_PACK_UB_PATH=$(find ${JUNIPER_PKG_PATH} -type f -name '*deb' )
 
 
-export JUNIPER_PKG_PATH="/storage/contrail/${CONTRAIL_VERSION}/"
-export CONTRAIL_PLUGIN_PACK_UB_PATH=$(find $JUNIPER_PKG_PATH -maxdepth 1 -name 'contrail-install-packages*.deb' -exec stat -c "%y %n" {} + | sort -r | head -n 1 | cut -d' ' -f 4)
+echo "Description string: ${TEST_GROUP} on ${NODE_NAME}: ${ENV_NAME}"
+
 source $VENV_PATH/bin/activate
 
 systest_parameters=''
-[[ $USE_SNAPSHOTS == "true"  ]] && systest_parameters+=' -k -K' || echo "Snapshots will be used for test and env will be stored after test"
+[[ $USE_SNAPSHOTS == "true"  ]] && systest_parameters+=' --k' || echo new env will not be created
+[[ $ERASE_AFTER  == "true"  ]] && echo the env will be erased after test || systest_parameters+=' -K'
 
-echo test-group: $TEST_GROUP
-echo env-name: $ENV_NAME
 echo use-snapshots: $USE_SNAPSHOTS
-echo fuel-release: $FUEL_RELEASE
 echo venv-path: $VENV_PATH
 echo env-name: $ENV_NAME
-echo iso-path: $ISO_PATH   
-echo plugin-path: $CONTRAIL_PLUGIN_PATH
-echo ubuntu-plugin-path: $CONTRAIL_PLUGIN_PACK_UB_PATH
-echo plugin-checksum: $(md5sum -b $DVS_PLUGIN_PATH)
+echo iso-path: $ISO_PATH
+echo plugin-path: $DVS_PLUGIN_PATH
 
-git log  --pretty=oneline | head
+echo plugin path: $DVS_PLUGIN_PATH plugin checksum: $(md5sum -b $DVS_PLUGIN_PATH)
 
-./plugin_test/utils/jenkins/system_tests.sh -t test ${systest_parameters} -i ${ISO_PATH} -j ${JOB_NAME} -o --group=${TEST_GROUP}
+cd plugin_test/
+#./utils/jenkins/system_tests.sh -t test -w $(pwd) -j contrail.bvt.ubuntu -i $ISO_PATH  -o --group=${TEST_GROUP}
+
+#[[ $SHUTDOWN_AFTER == "true" ]] && dos.py destroy ${ENV_NAME} || echo  the env will not be powered off after test
