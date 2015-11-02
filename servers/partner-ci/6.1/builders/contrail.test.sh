579535796c910d049fbbcd84ff20ae4634e8e8d1
Ruslan Khozinov <ruslan.khozinov@gmail.com>
2015-11-03 00:58:51 +0300

    contrail2.0
     - fuel-release variable's initialization is refactored
     - logic of smoke multijob is fixed
     - output of variables values is added
     - unused lines are removed

--- servers/partner-ci/6.1/builders/contrail.test.sh
+++ servers/partner-ci/6.1/builders/contrail.test.sh
@@ -1,15 +1,16 @@
-#!/bin/bash -e
+#!/bin/bash -xe
 
 export ISO_PATH="${ISO_STORAGE}/${ISO_FILE}"
 export ISO_VERSION=$(cut -d'-' -f3-3 <<< $ISO_FILE)
+export FUEL_RELEASE=$(cut -d'-' -f2-2 <<< $ISO_FILE | tr -d '.')
+
 export ENV_NAME="${ENV_PREFIX}.${ISO_VERSION}"
+export VENV_PATH="${HOME}/${FUEL_RELEASE}-venv"
 
-export CONTRAIL_PLUGIN_PATH=$(ls ${WORKSPACE}/fuel-plugin-contrail*.rpm)
-
-export JUNIPER_PKG_PATH="/storage/contrail/22/"
+export CONTRAIL_PLUGIN_PATH=$(ls ${WORKSPACE}/contrail*.rpm)
+export JUNIPER_PKG_PATH="/storage/contrail/2.20/"
 export CONTRAIL_PLUGIN_PACK_CEN_PATH=$(find ${JUNIPER_PKG_PATH} -type f -name '*rpm' )
 export CONTRAIL_PLUGIN_PACK_UB_PATH=$(find ${JUNIPER_PKG_PATH} -type f -name '*deb' )
-
 
 echo "Description string: ${TEST_GROUP} on ${NODE_NAME}: ${ENV_NAME}"
 
