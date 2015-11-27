c1e310803a3dc972bbf8bb7f782d7b7e1755fe04
Ruslan Khozinov <ruslan.khozinov@gmail.com>
2015-11-27 17:21:10 +0300

    6.1/dvs: some changes

--- servers/partner-ci/6.1/builders/dvs.test.sh
+++ servers/partner-ci/6.1/builders/dvs.test.sh
@@ -1,28 +1,29 @@
-#!/bin/bash -e 
+#!/bin/bash -xe 
 
 export ISO_PATH="${ISO_STORAGE}/${ISO_FILE}"
 export ISO_VERSION=$(cut -d'-' -f3-3 <<< $ISO_FILE) 
 export FUEL_RELEASE=$(cut -d'-' -f2-2 <<< $ISO_FILE | tr -d '.') 
-export DVS_PLUGIN_PATH=$(ls ${WORKSPACE}/fuel-plugin-vmware-dvs*.rpm)
 
 export ENV_NAME="${ENV_PREFIX}.${ISO_VERSION}"
 export VENV_PATH="${HOME}/${FUEL_RELEASE}-venv"
 
-
-echo "Description string: ${TEST_GROUP} on ${NODE_NAME}: ${ENV_NAME}"
+[[ -z "${DVS_PLUGIN_PATH}" ]] && export DVS_PLUGIN_PATH=$(ls ${WORKSPACE}/fuel-plugin-vmware-dvs*.rpm) \
+                            || true
 
 source $VENV_PATH/bin/activate
 
 systest_parameters=''
 [[ $USE_SNAPSHOTS == "true"  ]] && systest_parameters+=' -k' || echo new env will be created
-[[ $ERASE_AFTER  == "true"  ]] && echo the env will be erased after test || systest_parameters+=' -K' 
+[[ $ERASE_AFTER   == "true"  ]] && echo the env will be erased after test || systest_parameters+=' -K' 
 
+echo test-group: $TEST_GROUP
+echo env-name: $ENV_NAME
 echo use-snapshots: $USE_SNAPSHOTS
-
+echo fuel-release: $FUEL_RELEASE
 echo venv-path: $VENV_PATH
 echo env-name: $ENV_NAME
 echo iso-path: $ISO_PATH   
-echo plugin-path: $DVS_PLUGIN_PATH
-echo plugin path: $DVS_PLUGIN_PATH plugin checksum: $(md5sum -b $DVS_PLUGIN_PATH) 
+echo plugin-path: $DVS_PLUGIN_PATH 
+echo plugin-checksum: $(md5sum -b $DVS_PLUGIN_PATH) 
 
 ./plugin_test/utils/jenkins/system_tests.sh -t test ${systest_parameters} -i ${ISO_PATH} -j ${JOB_NAME} -o --group=${TEST_GROUP}
