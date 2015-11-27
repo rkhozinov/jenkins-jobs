a3dcf3a7337e28945ef72b05484f6a2021819ea1
Ruslan Khozinov <ruslan.khozinov@gmail.com>
2015-11-27 09:39:46 +0300

    modified:   7.0/builders/contrail.test.sh  - merge with 6.0 contrail builder

--- servers/partner-ci/7.0/builders/contrail.test.sh
+++ servers/partner-ci/7.0/builders/contrail.test.sh
@@ -1,13 +1,21 @@
 #!/bin/bash -e 
 
+[ -z $CONTRAIL_VERSION ] && exit 1 || echo contrail version is $CONTRAIL_VERSION
+
 export ISO_PATH="${ISO_STORAGE}/${ISO_FILE}"
 export ISO_VERSION=$(cut -d'-' -f3-3 <<< $ISO_FILE) 
+export FUEL_RELEASE=$(cut -d'-' -f2-2 <<< $ISO_FILE | tr -d '.') 
+
 export ENV_NAME="${ENV_PREFIX}.${ISO_VERSION}"
+export VENV_PATH="${HOME}/${FUEL_RELEASE}-venv"
 
-export CONTRAIL_PLUGIN_PATH=$(ls ${WORKSPACE}/fuel-plugin-contrail*.rpm)
+[[ -z ${CONTRAIL_PLUGIN_PATH} ]] && export CONTRAIL_PLUGIN_PATH=$(ls ${WORKSPACE}/contrail*.rpm) \
+                                 || echo CONTRAIL_PLUGIN_PATH=$CONTRAIL_PLUGIN_PATH
 
-export JUNIPER_PKG_PATH="/storage/contrail/22/"
-export CONTRAIL_PLUGIN_PACK_CEN_PATH=$(find ${JUNIPER_PKG_PATH} -type f -name '*rpm' )
+
+#export JUNIPER_PKG_PATH="/storage/contrail/2.20/"
+export JUNIPER_PKG_PATH="/storage/contrail/${CONTRAIL_VERSION}/"
+#export CONTRAIL_PLUGIN_PACK_CEN_PATH=$(find ${JUNIPER_PKG_PATH} -type f -name '*rpm' )
 export CONTRAIL_PLUGIN_PACK_UB_PATH=$(find ${JUNIPER_PKG_PATH} -type f -name '*deb' )
 
 
@@ -16,18 +24,11 @@ echo "Description string: ${TEST_GROUP} on ${NODE_NAME}: ${ENV_NAME}"
 source $VENV_PATH/bin/activate
 
 systest_parameters=''
-[[ $USE_SNAPSHOTS == "true"  ]] && systest_parameters+=' --k' || echo new env will not be created
+[[ $USE_SNAPSHOTS == "true"  ]] && systest_parameters+=' -k' || echo new env will be created
 [[ $ERASE_AFTER  == "true"  ]] && echo the env will be erased after test || systest_parameters+=' -K' 
 
 echo use-snapshots: $USE_SNAPSHOTS
 echo venv-path: $VENV_PATH
 echo env-name: $ENV_NAME
 echo iso-path: $ISO_PATH   
-echo plugin-path: $DVS_PLUGIN_PATH
-
-echo plugin path: $DVS_PLUGIN_PATH plugin checksum: $(md5sum -b $DVS_PLUGIN_PATH) 
-
-cd plugin_test/
-#./utils/jenkins/system_tests.sh -t test -w $(pwd) -j contrail.bvt.ubuntu -i $ISO_PATH  -o --group=${TEST_GROUP}
-
-#[[ $SHUTDOWN_AFTER == "true" ]] && dos.py destroy ${ENV_NAME} || echo  the env will not be powered off after test
+#./plugin_test/utils/jenkins/system_tests.sh -t test ${systest_parameters} -i ${ISO_PATH} -j ${JOB_NAME} -o --group=${TEST_GROUP}
