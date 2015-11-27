c5bddd11c3669d4c27cb0b67e18baa10137b9944
Ruslan Khozinov <ruslan.khozinov@gmail.com>
2015-11-27 23:07:04 +0300

    dvs,contrail: rebase changes modified:   7.0/builders/dvs.prepare.sh  - some conditions were refactored  - support of the DEBUG parameter was added modified:   7.0/builders/dvs.test.sh  - support of the DEBUG parameter was added  - clearing of rpms was removed  - fixed a bug with test run modified:   7.0/dvs.yaml  - the DEBUG parameter was added  - merge with 6.1

--- servers/partner-ci/7.0/builders/dvs.test.sh
+++ servers/partner-ci/7.0/builders/dvs.test.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -e 
 
+# activate bash xtrace for script
+[[ "${DEBUG}" == "true" ]] && set -x || set +x
+
 export ISO_PATH="${ISO_STORAGE}/${ISO_FILE}"
 export ISO_VERSION=$(cut -d'-' -f3-3 <<< $ISO_FILE) 
 export FUEL_RELEASE=$(cut -d'-' -f2-2 <<< $ISO_FILE | tr -d '.') 
-export DVS_PLUGIN_PATH=$(ls ${WORKSPACE}/fuel-plugin-vmware-dvs*.rpm)
+
+[[ -z "${DVS_PLUGIN_PATH}" ]] && export DVS_PLUGIN_PATH=$(ls ${WORKSPACE}/fuel-plugin-vmware-dvs*.rpm) || true
 
 export ENV_NAME="${ENV_PREFIX}.${ISO_VERSION}"
 export VENV_PATH="${HOME}/${FUEL_RELEASE}-venv"
-
 
 echo "Description string: ${TEST_GROUP} on ${NODE_NAME}: ${ENV_NAME}"
 
@@ -18,11 +21,11 @@ systest_parameters=''
 [[ $ERASE_AFTER  == "true"  ]] && echo the env will be erased after test || systest_parameters+=' -K' 
 
 echo use-snapshots: $USE_SNAPSHOTS
-
 echo venv-path: $VENV_PATH
 echo env-name: $ENV_NAME
 echo iso-path: $ISO_PATH   
 echo plugin-path: $DVS_PLUGIN_PATH
-echo plugin path: $DVS_PLUGIN_PATH plugin checksum: $(md5sum -b $DVS_PLUGIN_PATH) 
+echo plugin path: $DVS_PLUGIN_PATH 
+echo plugin checksum: $(md5sum -b $DVS_PLUGIN_PATH) 
 
 ./plugin_test/utils/jenkins/system_tests.sh -t test ${systest_parameters} -i ${ISO_PATH} -j ${JOB_NAME} -o --group=${TEST_GROUP}
