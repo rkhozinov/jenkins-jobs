82a26589422a36afb2083c790238fa09338a1d04
Ruslan Khozinov <ruslan.khozinov@gmail.com>
2015-12-03 21:49:09 +0300

    dvs: build merge
     - builders are merged
     - timers and jobs' state are moved to variables

--- servers/partner-ci/7.0/builders/dvs.test.sh
+++ servers/partner-ci/7.0/builders/dvs.test.sh
@@ -7,25 +7,25 @@ export ISO_PATH="${ISO_STORAGE}/${ISO_FILE}"
 export ISO_VERSION=$(cut -d'-' -f3-3 <<< $ISO_FILE) 
 export FUEL_RELEASE=$(cut -d'-' -f2-2 <<< $ISO_FILE | tr -d '.') 
 
-[[ -z "${DVS_PLUGIN_PATH}" ]] && export DVS_PLUGIN_PATH=$(ls ${WORKSPACE}/fuel-plugin-vmware-dvs*.rpm) || true
-
 export ENV_NAME="${ENV_PREFIX}.${ISO_VERSION}"
 export VENV_PATH="${HOME}/${FUEL_RELEASE}-venv"
 
-echo "Description string: ${TEST_GROUP} on ${NODE_NAME}: ${ENV_NAME}"
+[[ -z "${DVS_PLUGIN_PATH}" ]] && export DVS_PLUGIN_PATH=$(ls ${WORKSPACE}/fuel-plugin-vmware-dvs*.rpm) ||  echo DVS_PLUGIN_PATH=$DVS_PLUGIN_PATH
 
 source $VENV_PATH/bin/activate
 
 systest_parameters=''
 [[ $USE_SNAPSHOTS == "true"  ]] && systest_parameters+=' -k' || echo new env will be created
-[[ $ERASE_AFTER  == "true"  ]] && echo the env will be erased after test || systest_parameters+=' -K' 
+[[ $ERASE_AFTER   == "true"  ]] && echo the env will be erased after test || systest_parameters+=' -K'
 
+echo test-group: $TEST_GROUP
+echo env-name: $ENV_NAME
 echo use-snapshots: $USE_SNAPSHOTS
+echo fuel-release: $FUEL_RELEASE
 echo venv-path: $VENV_PATH
 echo env-name: $ENV_NAME
 echo iso-path: $ISO_PATH   
 echo plugin-path: $DVS_PLUGIN_PATH
-echo plugin path: $DVS_PLUGIN_PATH 
-echo plugin checksum: $(md5sum -b $DVS_PLUGIN_PATH) 
+echo plugin-checksum: $(md5sum -b $DVS_PLUGIN_PATH)
 
 ./plugin_test/utils/jenkins/system_tests.sh -t test ${systest_parameters} -i ${ISO_PATH} -j ${JOB_NAME} -o --group=${TEST_GROUP}
