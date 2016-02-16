24f654eb60f7e38c7082a5d5f85992c686fe9d37
Ruslan Khozinov <ruslan.khozinov@gmail.com>
2016-02-16 23:18:31 +0300

    continue fixing of reporter

--- servers/partner-ci/8.0/builders/reporter.sh
+++ servers/partner-ci/8.0/builders/reporter.sh
@@ -3,5 +3,5 @@ venv_path='/home/jenkins/trep'
 [ -d $venv_path ] && source $venv_path/bin/activate || (echo an venv is needed; exit 1)
 python setup.py install > /dev/null 2>/dev/null
 
-echo  $TREP_TESTRAIL_PLAN
+env
 trep
