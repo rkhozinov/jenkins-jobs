929758e7e42cd6a0e33a0dde50e4cf97679036f1
Ruslan Khozinov <ruslan.khozinov@gmail.com>
2016-02-29 19:11:36 +0300

    New build script for nsxv is created
    - the scripts fixes the problem with vendoring checks

--- servers/partner-ci/8.0/builders/build-plugin-nsxv.sh
+++ servers/partner-ci/8.0/builders/build-plugin-nsxv.sh
@@ -3,19 +3,21 @@ set -e
 
 git log  --pretty=oneline | head
 
-find . -name '*.erb' -print0 | xargs -0 -P1 -L1 -I '%' erb -P -x -T '-' % | ruby -c
-find . -name '*.pp' -print0 | xargs -0 -P1 -L1 puppet parser validate --verbose
-find . -name '*.pp' -print0 | xargs -0 -r -P1 -L1 puppet-lint \
-          --fail-on-warnings \
-          --with-context \
-          --with-filename \
-          --no-80chars-check \
-          --no-variable_scope-check \
-          --no-nested_classes_or_defines-check \
-          --no-autoloader_layout-check \
-          --no-class_inherits_from_params_class-check \
-          --no-documentation-check \
-          --no-arrow_alignment-check
+path="./deployment_scripts/puppet/manifests ./deployment_scripts/puppet/modules/nsxv"
+
+find $path -name '*.erb' -print0 | xargs -0 -P1 -L1 -I '%' erb -P -x -T '-' % | ruby -c
+find $path -name '*.pp' -print0 | xargs -0 -P1 -L1 puppet parser validate --verbose
+find $path -name '*.pp' -print0 | xargs -0 -r -P1 -L1 puppet-lint \
+         --fail-on-warnings \
+         --with-context \
+         --with-filename \
+         --no-80chars-check \
+         --no-variable_scope-check \
+         --no-nested_classes_or_defines-check \
+         --no-autoloader_layout-check \
+         --no-class_inherits_from_params_class-check \
+         --no-documentation-check \
+         --no-arrow_alignment-check
 
 fpb --check  ./
 fpb --build  ./
