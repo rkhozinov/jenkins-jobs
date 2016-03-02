c50719062db80d1ff793eb083468a468bda3759b
Ruslan Khozinov <ruslan.khozinov@gmail.com>
2016-03-02 18:13:06 +0300

    Package build is improved
     - 9.0.nsxv
     - submodule init is removed
     - linters are improved

--- servers/partner-ci/9.0/builders/build-plugin-nsxv.sh
+++ servers/partner-ci/9.0/builders/build-plugin-nsxv.sh
@@ -3,9 +3,11 @@ set -e
 
 git log  --pretty=oneline | head
 
-find . -name '*.erb' -print0 | xargs -0 -P1 -L1 -I '%' erb -P -x -T '-' % | ruby -c
-find . -name '*.pp' -print0 | xargs -0 -P1 -L1 puppet parser validate --verbose
-find ./deployment_scripts/puppet/manifests ./deployment_scripts/puppet/modules/nsxv -name '*.pp' -print0 | xargs -0 -r -P1 -L1 puppet-lint \
+path="./deployment_scripts/puppet/manifests ./deployment_scripts/puppet/modules/nsxv"
+
+find $path -name '*.erb' -print0 | xargs -0 -P1 -L1 -I '%' erb -P -x -T '-' % | ruby -c
+find $path -name '*.pp' -print0 | xargs -0 -P1 -L1 puppet parser validate --verbose
+find $path -name '*.pp' -print0 | xargs -0 -r -P1 -L1 puppet-lint \
          --fail-on-warnings \
          --with-context \
          --with-filename \
