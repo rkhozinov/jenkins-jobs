740bd45eefa024451a60ab7dfa15d4888c3f9b97
Ruslan Khozinov <ruslan.khozinov@gmail.com>
2016-04-10 00:02:12 +0300

    9.0 ci initialize

--- servers/partner-ci/9.0/builders/build-plugin-dvs.sh
+++ servers/partner-ci/9.0/builders/build-plugin-dvs.sh
@@ -3,7 +3,7 @@ set -e
 
 git log  --pretty=oneline | head
 
-path="./deployment_scripts/puppet/manifests ./deployment_scripts/puppet/modules/nsxv"
+path="./deployment_scripts/puppet/manifests ./deployment_scripts/puppet/modules/vmware_dvs"
 
 find $path -name '*.erb' -print0 | xargs -0 -P1 -L1 -I '%' erb -P -x -T '-' % | ruby -c
 find $path -name '*.pp' -print0 | xargs -0 -P1 -L1 puppet parser validate --verbose
@@ -20,11 +20,7 @@ find $path -name '*.pp' -print0 | xargs -0 -r -P1 -L1 puppet-lint \
          --no-arrow_alignment-check
 
 fpb --check  ./
-fpb --debug --build  ./
+fpb --build  ./
 
 pkg_name=$(ls -t *.rpm | head -n1)
 mv $pkg_name $(echo $pkg_name | head -n 1 | sed s/.noarch/-$BUILD_NUMBER.noarch/)
-
-
-
-
