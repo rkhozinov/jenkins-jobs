436dae739f820ba4193dbfe2d404c60504eb6dad
Ruslan Khozinov <ruslan.khozinov@gmail.com>
2016-04-27 00:22:21 +0300

    update_master variable was removed for dvs2.0

--- servers/partner-ci/builders/common.prepare.sh
+++ servers/partner-ci/builders/common.prepare.sh
@@ -1,58 +1,35 @@
 #!/bin/bash -e
 
 git log  --pretty=oneline | head
-IS_NESTED=$(cat /sys/module/kvm_intel/parameters/nested)
-IS_IGNORE_MSRS=$(cat /sys/module/kvm/parameters/ignore_msrs)
-IS_EPT=$(cat /sys/module/kvm_intel/parameters/ept)
-echo 'nested option enable = '$IS_NESTED
-echo 'ignore msrs option enable = '$IS_IGNORE_MSRS
-echo 'ept option enable = '$IS_EPT
-
-if [[ $IS_NESTED == *"Y"* ]] && [[ $IS_IGNORE_MSRS == *"Y"* ]] && [[ $IS_EPT == *"Y"* ]]; then
-  echo 'nested kvm virtuallization succesfully enabled '
-else
-  echo 'nested kvm virtualization disabled or works incorrect, please check configuration'
-fi
 
 # activate bash xtrace for script
 [[ "${DEBUG}" == "true" ]] && set -x || set +x
 
-[ -z $ISO_FILE ] && (echo "ISO_FILE variable is empty"; exit 1)
-
-
-if [[ $ISO_FILE == *"Mirantis"* ]]; then
-  export FUEL_RELEASE=$(echo $ISO_FILE | cut -d- -f2 | tr -d '.iso')
-fi
-if [[ $ISO_FILE == *"custom"* ]]; then
-  export FUEL_RELEASE=90
-fi
-
-if [[ "${UPDATE_MASTER}" == "true" ]] && [[ ${FUEL_RELEASE} != *"80"* ]]; then
-  [ ${SNAPSHOTS_ID} ] && export SNAPSHOTS_ID=${SNAPSHOTS_ID} || export SNAPSHOTS_ID=${CUSTOM_VERSION:10}
-else
-  export SNAPSHOTS_ID="released"
+ISO_FILE_ARTIFACT='iso_file'
+# if user entered custom iso we should use it
+if [ -z $ISO_FILE  ]; then
+   # but if use doesn't want custom iso, we should get iso from artifacts
+   [ -f $ISO_FILE_ARTIFACT ] && source $ISO_FILE_ARTIFACT || (echo "There's not iso_file"; exit 1)
+   # check variable again - it shouldn't be empty
+   [ -z $ISO_FILE ] && (echo "ISO_FILE variable is empty"; exit 1)
 fi
 
-[ -z "${SNAPSHOTS_ID}" ] && { echo SNAPSHOTS_ID is empty; exit 1; }
 #remove old logs and test data
 [ -f nosetest.xml ] && rm -f nosetests.xml
-[ -d logs ] && rm -rf logs/* || mkdir logs
+rm -rf logs/*
 
 export ISO_PATH="${ISO_STORAGE}/${ISO_FILE}"
-[ ! -f $ISO_PATH ] && { echo "The $ISO_PATH isn't exist"; exit 1; }
-
-if [[ $ISO_FILE == *"Mirantis"* ]]; then
-  export FUEL_RELEASE=$(echo $ISO_FILE | cut -d- -f2 | tr -d '.iso')
-fi
-
-if [[ $ISO_FILE == *"custom"* ]]; then
-  export FUEL_RELEASE=90
-fi
-
+[ ! -f $ISO_PATH ] && (echo "The $ISO_PATH isn't exist"; exit 1)
+export ISO_VERSION=$(echo $ISO_FILE | cut -d'-' -f3-3 | tr -d '.iso' )
+echo iso build number is $ISO_VERSION
 export REQUIRED_FREE_SPACE=200
-
-export ENV_NAME="${ENV_PREFIX}.${SNAPSHOTS_ID}"
+export FUEL_RELEASE=$(cut -d'-' -f2-2 <<< $ISO_FILE | tr -d '.')
 export VENV_PATH="${HOME}/${FUEL_RELEASE}-venv"
+
+export ENV_NAME="${ENV_PREFIX}.${ISO_VERSION}"
+echo iso-version: $ISO_VERSION
+echo fuel-release: $FUEL_RELEASE
+echo virtual-env: $VENV_PATH
 
 ## For plugins we should get a valid version of requrements of python-venv
 ## This requirements could be got from the github repo
@@ -70,12 +47,30 @@ esac
 
 REQS_PATH="https://raw.githubusercontent.com/openstack/fuel-qa/${REQS_BRANCH}/fuelweb_test/requirements.txt"
 
-REQS_PATH_DEVOPS="https://raw.githubusercontent.com/openstack/fuel-qa/${REQS_BRANCH}/fuelweb_test/requirements-devops-source.txt"
-
 ###############################################################################
 
 ## We have limited disk resources, so before run of system tests a lab
 ## may have many deployed and runned envs, those may cause errors during test
+
+function dospy {
+  env_list=$1
+  action=$2
+
+  if [[ ! -z "${env_list}" ]] && [[ ! -z "${action}" ]]; then
+    for env in $env_list; do dos.py $action $env; done
+  fi
+}
+
+## Gets dospy environments
+## with prefix the function returns all env except envs like prefix
+
+function dospy_list {
+  prefix=$1
+  dos.py sync
+  [ -z $prefix ] && \
+    echo $(dos.py list | tail -n +3) || \
+    echo $(dos.py list | tail -n +3  | grep -v $prefix)
+}
 
 ## Recreate all an virtual env
 function recreate_venv {
@@ -84,176 +79,67 @@ function recreate_venv {
 }
 
 function get_venv_requirements {
-  rm -f requirements.*
-  wget --no-check-certificate -O requirements.txt $REQS_PATH
+  rm -f requirements.txt*
+  wget $REQS_PATH
   export REQS_PATH="$(pwd)/requirements.txt"
-  wget --no-check-certificate -O requirements-devops-source.txt $REQS_PATH_DEVOPS
-  export REQS_PATH_DEVOPS="$(pwd)/requirements-devops-source.txt"
-  export SPEC_REQS_PATH="${WORKSPACE}/plugin_test/requirement.txt"
-  sed -i 's/2.9.23/release\/2.9/g' $REQS_PATH_DEVOPS
-}
 
+  if [[ "${REQS_BRANCH}" == "stable/8.0" ]]; then
+    # bug: https://bugs.launchpad.net/fuel/+bug/1528193
+    sed -i 's/python-neutronclient.*/python-neutronclient==3.1.0/' $REQS_PATH
+  fi
+  ## change version for some package
+  #if [[ "${REQS_BRANCH}" != "master" ]]; then
+  #  # bug: https://bugs.launchpad.net/fuel/+bug/1528193
+  #  sed -i 's/python-novaclient>=2.15.0/python-novaclient==2.35.0/' $REQS_PATH
+  #fi
+}
+   
 function prepare_venv {
     source "${VENV_PATH}/bin/activate"
-    easy_install -U pip
-    export redirected_output='pip.properties'
-    pip install -r "${REQS_PATH}" --upgrade > $redirected_output
-    pip install -r "${REQS_PATH_DEVOPS}" --upgrade > $redirected_output
-    [ -e $SPEC_REQS_PATH ] && pip install -r "${SPEC_REQS_PATH}" --upgrade > $redirected_output
+    pip --version
+    [ $? -ne 0 ] && easy_install -U pip
+    if [[ "${DEBUG}" == "true" ]]; then
+        pip install -r "${REQS_PATH}" --upgrade > /dev/null 2>/dev/null
+    else
+        pip install -r "${REQS_PATH}" --upgrade > /dev/null 2>/dev/null
+    fi
+
     django-admin.py syncdb --settings=devops.settings --noinput
     django-admin.py migrate devops --settings=devops.settings --noinput
     deactivate
 }
 
-function smart_erase {
-  env=$1
-
-  virsh list --all --name | grep $env && vms=$(virsh list --all --name | grep $env) || echo "there is no vms"
-
-  virsh net-list | tail -n +3 | cut -d' ' -f2-2 | grep $env && networks=$(virsh net-list | tail -n +3 | cut -d' ' -f2-2 | grep $env) \
-  || echo "there is no networks"
-  if [ ! -z "$networks" ]; then
-    for net in $networks; do
-      if virsh net-destroy $net; then
-        echo "network destroyed succesfully"
-      else
-        ref=$?
-        echo "there are some troubles with virt-networks stack, please check ( exit code = $ref )"
-      fi
-      if virsh net-undefine $net; then
-        echo "network destroyed succesfully"
-      else
-        ref=$?
-        echo "there are some troubles with virt-networks stack, please check ( exit code = $ref )"
-      fi
-    done
-  fi
-  for vm in $vms; do
-    domstat=$(virsh domstate $vm)
-    if [[ $domstat != *"shut"* ]]; then
-      if virsh destroy $vm; then
-        echo "domain destroyed succesfully"
-        virsh undefine --remove-all-storage --snapshots-metadata $vm
-      else
-        ref=$?
-        echo "there are some troubles with virt stack, restart services and recheck"
-	sudo service libvirt-bin restart
-        if virsh destroy $vm; then
-          echo "domain destroyed succesfully"
-          virsh undefine --remove-all-storage --snapshots-metadata $vm
-        else
-          ref=$?
-          echo "there are some troubles with virt stack, please check it manually "
-        fi
-      fi
-    else
-      virsh undefine --remove-all-storage --snapshots-metadata $vm
-    fi
-  done
-  dos.py sync
+function fix_logger {
+   config_path="${HOME}/.devops/log.yaml"
+   echo devops config path $config_path
+   sed -i '/disable_existing_loggers.*/d' $config_path
+   echo disable_existing_loggers: False >> $config_path
 }
 
-#################################################################
-## Gets dospy environments
-## with prefix the function returns all env except envs like prefix
-function dospy_list {
-  prefix=$1
-  dos.py sync
-  [ -z $prefix ] && \
-    echo $(dos.py list | tail -n +3) || \
-    echo $(dos.py list | tail -n +3  | grep $prefix)
-}
 
-##################################################################
-wait_ws() {
-  while [ $(pgrep vmrun | wc -l) -ne 0 ] ; do sleep 5; done
-}
+####################################################################################
 
 [[ "${RECREATE_VENV}" == "true" ]] && recreate_venv
 
 get_venv_requirements
 
-[ -d $VENV_PATH ] && prepare_venv || { echo "$VENV_PATH doesn't exist $VENV_PATH will be recreated"; recreate_venv; }
+[ -d $VENV_PATH ] && prepare_venv || { echo "$VENV_PATH doesn't exist $VENV_PATH will be recreated"; recreate_venv; } 
 
+fix_logger
 
 source "$VENV_PATH/bin/activate"
 
 [ -z $VIRTUAL_ENV ] && { echo "VIRTUAL_ENV is empty"; exit 1; }
 
-if [[ "${FORCE_ERASE}" == "true" ]]; then
-  for env in $(dospy_list); do
-    smart_erase $env
-  done
+if [[ "${FORCE_ERASE}" -eq "true" ]]; then
+  dospy $(dospy_list) erase
 else
   # determine free space before run the cleaner
   free_space=$(df -h | grep '/$' | awk '{print $4}' | tr -d G)
-=======
-set +x
 
-cmd="vmrun -T ws-shared -h https://localhost:443/sdk \
--u ${WORKSTATION_USERNAME} -p ${WORKSTATION_PASSWORD}"
-nodes=${WORKSTATION_NODES}
-snapshot="${WORKSTATION_SNAPSHOT}"
+  (( $free_space < $REQUIRED_FREE_SPACE )) && dospy $(dospy_list $ENV_NAME) erase  || echo "free-space: $free_space"
 
-# start from saved state
-for node in $nodes; do
-  if [[ $(pgrep vmrun | wc -l) -ne 0 ]]; then
-    echo "Stopping $node"
-    $cmd stop "[standard] $node/$node.vmx" || \
-      echo "Error: $node failed to stop" &
-  fi
-done
-wait_ws
-set -x
-if [[ "${FORCE_REUSE}" == "false" ]]; then
-  if [[ "${FORCE_ERASE}" == "true" ]]; then
-    for env in $(dospy_list); do
-      smart_erase $env
-    done
-  else
-    # determine free space before run the cleaner
-    free_space=$(df -h | grep '/$' | awk '{print $4}' | tr -d G)
+  # poweroff all envs
+  dospy $(dospy_list) destroy
 
-    if (( $free_space < $REQUIRED_FREE_SPACE )); then
-      for env in $(dospy_list $ENV_NAME); do
-        if [[ $env  != *"released"* ]]; then
-          smart_erase $env
-        fi
-      done
-    fi
-  fi
-  ###############################################################
-  ##############possibility of reusing envs######################
-  current_date=$(date +'%Y-%m-%d')
-  mod_current_date=$(date -d $current_date +"%Y%m%d")
-  for env in $(dospy_list $ENV_NAME); do
-    if [[ $env  == $ENV_NAME ]] && [[ $env  != *"released"* ]]; then
-      if [[ $env  != *"released"* ]]; then
-        if dos.py snapshot-list $env | grep empty; then
-          snap_date=$(dos.py snapshot-list $env | grep empty | awk '{print $2}')
-          mod_snap_date=$(date -d $snap_date +"%Y%m%d")
-          if [[ $mod_snap_date -eq $mod_current_date ]]; then
-            echo "$env is suitable for test, it will be reused"
-            USEFUL_ENV=$env
-          else
-            echo "$env is not suitable for test, it will be erased"
-            smart_erase $env
-          fi
-        else
-          echo "there is no date-metadata, $env will be erased"
-          smart_erase $env
-        fi
-      else
-        echo "there're no snapshots to reuse"
-      fi
-    fi
-  done
-
-  for env in $(dospy_list); do
-    [[ $env  != $USEFUL_ENV ]] && [[ $env  != *"released"* ]] && smart_erase $env
-  done
 fi
-##########################################################
-[[ "${DEBUG}" == "true" ]] && virsh list --all
-sudo cp /var/log/libvirt/libvirtd.log ${WORKSPACE}/libvirtd_before_test.log
-sudo chown jenkins:jenkins ${WORKSPACE}/libvirtd_before_test.log
--- servers/partner-ci/builders/contrail.test.sh
+++ servers/partner-ci/builders/contrail.test.sh
@@ -3,117 +3,28 @@
 # activate bash xtrace for script
 [[ "${DEBUG}" == "true" ]] && set -x || set +x
 
-[[ "${FORCE_VSRX_COPY}" == "true" ]] && sudo rm -rf $VSRX_TARGET_IMAGE_PATH
-sudo cp $VSRX_ORIGINAL_IMAGE_PATH $VSRX_TARGET_IMAGE_PATH
-
-[ $CONTRAIL_VERSION ] && echo "contrail version is $CONTRAIL_VERSION" \
-                      || { echo "CONTRAIL_VERSION is not defined";  exit 1; }
+[ -z $CONTRAIL_VERSION ] && exit 1 || echo contrail version is $CONTRAIL_VERSION
 
 export ISO_PATH="${ISO_STORAGE}/${ISO_FILE}"
-[ -z $ISO_PATH  ] && { echo "ISO_PATH is empty or doesn't exist"; exit 1; }
+export ISO_VERSION=$(cut -d- -f3-3 <<< $ISO_FILE) 
+export FUEL_RELEASE=$(cut -d- -f2-2 <<< $ISO_FILE | tr -d '.') 
 
-if [[ $ISO_FILE == *"custom"* ]]; then
-  export FUEL_RELEASE=90
-else
-  export FUEL_RELEASE=$(echo $ISO_FILE | cut -d- -f2 | tr -d '.iso')
-fi
-
-if [ "${SNAPSHOTS_ID}" != "released" ]; then
-  if [[ "${UPDATE_MASTER}" == "true" ]] && [[ ${FUEL_RELEASE} != *"80"* ]]; then
-    if [ -f $SNAPSHOT_OUTPUT_FILE ]; then
-      . $SNAPSHOT_OUTPUT_FILE
-      export EXTRA_RPM_REPOS
-      export UPDATE_FUEL_MIRROR
-      export EXTRA_DEB_REPOS
-    else
-      echo "SNAPSHOT_OUTPUT_FILE is empty or doesn't exist"
-      exit 1
-    fi
-  else
-    export SNAPSHOTS_ID="released"
-  fi
-fi
-
-if [[ $SNAPSHOTS_ID == *"lastSuccessfulBuild"* ]]; then
-  export SNAPSHOTS_ID=$(cat snapshots.params | grep -Po '#\K[^ ]+')
-fi
-
-[ -z "${SNAPSHOTS_ID}" ] && { echo SNAPSHOTS_ID is empty; exit 1; }
-
-
-if [ -z "${PKG_JOB_BUILD_NUMBER}" ]; then
-    if [ -f build.properties ]; then
-        export PKG_JOB_BUILD_NUMBER=$(grep "BUILD_NUMBER" < build.properties | cut -d= -f2 )
-    else
-        echo "build.properties file is not available so the results couldn't be publihsed"
-        echo "$PKG_JOB_BUILD_NUMBER is empty, but it's needed for reporter. Exit."
-        exit 1
-    fi
-fi
-
-#remove old logs and test data
-[ -f nosetest.xml ] && sudo rm -f nosetests.xml
-sudo rm -rf logs/*
-
-if [[ $ISO_FILE == *"Mirantis"* ]]; then
-  export FUEL_RELEASE=$(echo $ISO_FILE | cut -d- -f2 | tr -d '.iso')
-fi
-
-export ENV_NAME="${ENV_PREFIX}.${SNAPSHOTS_ID}"
+export ENV_NAME="${ENV_PREFIX}.${ISO_VERSION}"
 export VENV_PATH="${HOME}/${FUEL_RELEASE}-venv"
 
 [[ -z ${CONTRAIL_PLUGIN_PATH} ]] && export CONTRAIL_PLUGIN_PATH=$(ls -t ${WORKSPACE}/contrail*.rpm | head -n 1) \
-                                 || echo "CONTRAIL_PLUGIN_PATH=$CONTRAIL_PLUGIN_PATH"
+                                 || echo CONTRAIL_PLUGIN_PATH=$CONTRAIL_PLUGIN_PATH
 
 
 export JUNIPER_PKG_PATH="/storage/contrail/${CONTRAIL_VERSION}/"
-[ -d $JUNIPER_PKG_PATH ] && echo "JUNIPER_PKG_PATH is $JUNIPER_PKG_PATH" \
-                         || { echo "$JUNIPER_PKG_PATH is not found";  exit 1; }
-
-[ -z $CONTRAIL_PLUGIN_PACK_UB_PATH ] && \
-  export CONTRAIL_PLUGIN_PACK_UB_PATH=$(find $JUNIPER_PKG_PATH -maxdepth 1 -name 'contrail-install-packages*.deb' -exec stat -c "%y %n" {} + | sort -r | head -n 1 | cut -d' ' -f 4)
-[ -f $CONTRAIL_PLUGIN_PACK_UB_PATH ] && echo "CONTRAIL_PLUGIN_PACK_UB_PATH is $CONTRAIL_PLUGIN_PACK_UB_PATH" \
-                         || { echo "CONTRAIL_PLUGIN_PACK_UB_PATH is not found";  exit 1; }
+export CONTRAIL_PLUGIN_PACK_UB_PATH=$(find $JUNIPER_PKG_PATH -maxdepth 1 -name 'contrail-install-packages*.deb' -exec stat -c "%y %n" {} + | sort -r | head -n 1 | cut -d' ' -f 4)
 export JUNIPER_PKG_VERSION=$(sed 's/[-_~]/-/g' <<< ${CONTRAIL_PLUGIN_PACK_UB_PATH} | cut -d- -f4-5)
+source $VENV_PATH/bin/activate
 
-if [[ $VCENTER_USE == "true"  ]]; then
+systest_parameters=''
+[[ $USE_SNAPSHOTS == "true"  ]] && systest_parameters+=' -k' || echo new env will be created
+[[ $ERASE_AFTER   == "true"  ]] && echo the env will be erased after test || systest_parameters+=' -K'
 
-  add_interface_to_bridge() {
-    env=$1
-    net_name=$2
-    nic=$3
-
-    for net in $(virsh net-list | grep ${env}_${net_name} | awk '{print $1}'); do
-      bridge=$(virsh net-info $net | grep -i bridge |awk '{print $2}')
-      setup_bridge $bridge $nic && echo $net_name bridge $bridge ready
-    done
-  }
-
-  setup_bridge() {
-    set -x
-    bridge=$1
-    nic=$2
-
-    [[ "${DISABLE_STP}" == "true" ]] && sudo brctl stp $bridge off
-    sudo brctl addif $bridge $nic
-
-    sudo ip address flush $nic
-    sudo ip link set dev $nic up
-    sudo ip link set dev $bridge up
-
-    if sudo /sbin/iptables-save | grep $bridge | grep -i reject | grep -q FORWARD; then
-      sudo /sbin/iptables -D FORWARD -o $bridge -j REJECT --reject-with icmp-port-unreachable
-      sudo /sbin/iptables -D FORWARD -i $bridge -j REJECT --reject-with icmp-port-unreachable
-    fi
-
-  }
-
-  clean_iptables() {
-    sudo /sbin/iptables -F
-    sudo /sbin/iptables -t nat -F
-    sudo /sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-  }
-fi
 
 echo test-group: $TEST_GROUP
 echo env-name: $ENV_NAME
@@ -121,62 +32,20 @@ echo use-snapshots: $USE_SNAPSHOTS
 echo fuel-release: $FUEL_RELEASE
 echo venv-path: $VENV_PATH
 echo env-name: $ENV_NAME
-echo iso-path: $ISO_PATH
+echo iso-path: $ISO_PATH   
 echo plugin-path: $CONTRAIL_PLUGIN_PATH
 echo ubuntu-plugin-path: $CONTRAIL_PLUGIN_PACK_UB_PATH
 echo juniper-package-version: $JUNIPER_PKG_VERSION
-echo plugin-checksum: $(md5sum -b $CONTRAIL_PLUGIN_PATH)
+echo plugin-checksum: $(md5sum -b $DVS_PLUGIN_PATH)
 
 cat << REPORTER_PROPERTIES > reporter.properties
 ISO_VERSION=$ISO_VERSION
 ISO_FILE=$ISO_FILE
-TEST_GROUP=$TEST_GROUP
 TEST_JOB_NAME=$JOB_NAME
 TEST_JOB_BUILD_NUMBER=$BUILD_NUMBER
 PKG_JOB_BUILD_NUMBER=$PKG_JOB_BUILD_NUMBER
 PLUGIN_VERSION=$PLUGIN_VERSION
 JUNIPER_PKG_VERSION=$JUNIPER_PKG_VERSION
-TREP_TESTRAIL_SUITE=$TREP_TESTRAIL_SUITE
-TREP_TESTRAIL_SUITE_DESCRIPTION=$TREP_TESTRAIL_SUITE_DESCRIPTION
-TREP_TESTRAIL_PLAN=$TREP_TESTRAIL_PLAN
-TREP_TESTRAIL_PLAN_DESCRIPTION=$TREP_TESTRAIL_PLAN_DESCRIPTION
-DATE=$(date +'%B-%d')
 REPORTER_PROPERTIES
 
-source $VENV_PATH/bin/activate
-#./plugin_test/utils/jenkins/system_tests.sh -t test ${systest_parameters} -i ${ISO_PATH} -j ${JOB_NAME} -o --group=${TEST_GROUP}
-
-# run python test set to create environments, deploy and test product
-# WORKAROUND for https://bugs.launchpad.net/fuel/+bug/1642991
-export WORKSPACE="${WORKSPACE}/plugin_test/fuel-qa"
-export PLUGIN_WORKSPACE="${WORKSPACE/\/fuel-qa}"
-export PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}${WORKSPACE}:${PLUGIN_WORKSPACE}"
-[[ "${DEBUG}" == "true" ]] && echo "PYTHONPATH:${PYTHONPATH} PATH:${PATH} WORKSPACE:${WORKSPACE}"
-python $PLUGIN_WORKSPACE/run_tests.py -q --nologcapture --with-xunit --group=${TEST_GROUP} &
-export SYSTEST_PID=$!
-	
-if [[ $VCENTER_USE == "true"  ]]; then
-  #Wait before environment is created
-  while [ true ]; do
-    [ $(virsh net-list | grep $ENV_NAME | wc -l) -eq 5 ] && break || sleep 10
-    [ -e /proc/$SYSTEST_PID ] && continue || \
-      { echo System tests exited prematurely, aborting; exit 1; }
-  done
-
-  [[ "${CLEAN_IPTABLES}" == "true" ]] && clean_iptables
-
-  for iface_map in $(echo $WORKSTATION_IFS | tr ',' ' '); do
-      iface=$(echo $iface_map | cut -d: -f1)
-      network=$(echo $iface_map | cut -d: -f2)
-      add_interface_to_bridge $ENV_NAME $network $iface
-  done
-fi
-
-echo "Waiting for system tests to finish"
-wait $SYSTEST_PID
-export RESULT=$?
-
-echo "ENVIRONMENT NAME is $ENV_NAME"
-dos.py list --ips | grep ${ENV_NAME}
-
-[ $RESULT -eq 0 ] && { echo "Tests succeeded"; exit $RESULT; }
+./plugin_test/utils/jenkins/system_tests.sh -t test ${systest_parameters} -i ${ISO_PATH} -j ${JOB_NAME} -o --group=${TEST_GROUP}
