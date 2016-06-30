e6b22f1d401f8d60bc8eb2ed4f3ca2ffbac4b3ae
naumvd95 <naumvd95@gmail.com>
2016-06-30 16:51:26 +0300

    added check-parameter for ignore files in dvs 9.0

--- servers/partner-ci/builders/build-plugin-dvs.sh
+++ servers/partner-ci/builders/build-plugin-dvs.sh
@@ -3,11 +3,19 @@ set -e
 
 git log  --pretty=oneline | head
 
-path="./deployment_scripts/puppet/manifests ./deployment_scripts/puppet/modules/vmware_dvs"
+cmd1="find $PUPPETLINT_PATH -name '*.erb' -print0 "
+cmd2="find $PUPPETLINT_PATH -name '*.pp'  -print0 "
+cmd3="find $PUPPETLINT_PATH -name '*.pp'  -print0 "
 
-find $path -name '*.erb' -print0 | xargs -0 -P1 -L1 -I '%' erb -P -x -T '-' % | ruby -c
-find $path -name '*.pp' -print0 | xargs -0 -P1 -L1 puppet parser validate --verbose
-find $path -name '*.pp' -print0 | xargs -0 -r -P1 -L1 puppet-lint \
+if [ ! -z "${PUPPETLINT_IGNORE}" ]; then
+  cmd1="find $PUPPETLINT_PATH -name '*.erb' ! -name $PUPPETLINT_IGNORE -print0 "
+  cmd2="find $PUPPETLINT_PATH -name '*.pp'  ! -name $PUPPETLINT_IGNORE -print0 "
+  cmd3="find $PUPPETLINT_PATH -name '*.pp'  ! -name $PUPPETLINT_IGNORE -print0 "
+fi
+
+$cmd1 | xargs -0 -P1 -L1 -I '%' erb -P -x -T '-' % | ruby -c
+$cmd2 | xargs -0 -P1 -L1 puppet parser validate --verbose
+$cmd3 | xargs -0 -r -P1 -L1 puppet-lint \
          --fail-on-warnings \
          --with-context \
          --with-filename \
@@ -18,6 +26,7 @@ find $path -name '*.pp' -print0 | xargs -0 -r -P1 -L1 puppet-lint \
          --no-class_inherits_from_params_class-check \
          --no-documentation-check \
          --no-arrow_alignment-check
+
 
 fpb --check  ./
 if [[ "${DEBUG}" == "true" ]]; then
