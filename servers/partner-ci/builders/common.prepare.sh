a0abdd086e2026bf5f6f304e2b122871f645ba57
Ruslan Khozinov <ruslan.khozinov@gmail.com>
2016-07-21 14:52:31 +0000

    common.prepare.sh edited online with Bitbucket

--- servers/partner-ci/builders/common.prepare.sh
+++ servers/partner-ci/builders/common.prepare.sh
@@ -36,14 +36,15 @@ export ISO_PATH="${ISO_STORAGE}/${ISO_FILE}"
 if [[ $ISO_FILE == *"mos"* ]] || [[ $ISO_FILE == *"kilo"* ]]; then 
   export ISO_VERSION=$(echo $ISO_FILE | cut -d'-' -f4-4 | tr -d '.iso' )
 elif [[ $ISO_FILE == *"Mirantis"* ]]; then
-  export ISO_VERSION=$(echo $ISO_FILE | cut -d'.' -f1-2 )
+  export ISO_VERSION=$(echo $ISO_FILE | tr -d '.iso' )
+  export FUEL_RELEASE=$(echo $ISO_VERSION | cut -d- -f2)
 else
   export ISO_VERSION=$(echo $ISO_FILE | cut -d'-' -f3-3 | tr -d '.iso' )
 fi
 
 echo iso build number is $ISO_VERSION
 export REQUIRED_FREE_SPACE=200
-export FUEL_RELEASE=$(cut -d'-' -f2-2 <<< $ISO_FILE | tr -d '.')
+
 export VENV_PATH="${HOME}/${FUEL_RELEASE}-venv"
 
 export ENV_NAME="${ENV_PREFIX}.${ISO_VERSION}"
