fa7b9a84e0d0df603ef29e8cee04a62fc5f47eb4
Ruslan Khozinov <ruslan.khozinov@gmail.com>
2016-07-21 14:55:17 +0000

    nsxv.test.sh edited online with Bitbucket

--- servers/partner-ci/builders/nsxv.test.sh
+++ servers/partner-ci/builders/nsxv.test.sh
@@ -35,12 +35,12 @@ fi
 [ -f nosetest.xml ] && rm -f nosetests.xml
 rm -rf logs/*
 
-export FUEL_RELEASE=$(cut -d- -f2-2 <<< ${ISO_FILE} | tr -d '.')
 export ISO_PATH="${ISO_STORAGE}/${ISO_FILE}"
 if [[ $ISO_FILE == *"mos"* ]] || [[ $ISO_FILE == *"kilo"* ]];then
   export ISO_VERSION=$(echo $ISO_FILE | cut -d'-' -f4-4 | tr -d '.iso' )
 elif [[ $ISO_FILE == *"Mirantis"* ]]; then
-    export ISO_VERSION=$(echo $ISO_FILE | cut -d'.' -f1-2 )
+  export ISO_VERSION=$(echo $ISO_FILE | tr -d '.iso' )
+  export FUEL_RELEASE=$(echo $ISO_VERSION | cut -d- -f2)
 else
   export ISO_VERSION=$(echo $ISO_FILE | cut -d'-' -f3-3 | tr -d '.iso' )
 fi
