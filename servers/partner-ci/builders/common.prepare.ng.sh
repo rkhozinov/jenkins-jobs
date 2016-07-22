4f1844807786ceaf930578987782fa4a8d49678d
Ruslan Khozinov <ruslan.khozinov@gmail.com>
2016-07-22 05:45:13 +0300

    vcenter: new generation of job to support cd

---
+++ servers/partner-ci/builders/common.prepare.ng.sh
@@ -0,0 +1,150 @@
+#!/bin/bash -e
+
+git log  --pretty=oneline | head
+IS_NESTED=$(cat /sys/module/kvm_intel/parameters/nested)
+IS_IGNORE_MSRS=$(cat /sys/module/kvm/parameters/ignore_msrs)
+IS_EPT=$(cat /sys/module/kvm_intel/parameters/ept)
+echo 'nested option enable = '$IS_NESTED
+echo 'ignore msrs option enable = '$IS_IGNORE_MSRS
+echo 'ept option enable = '$IS_EPT
+
+if [[ $IS_NESTED == *"Y"* ]] && [[ $IS_IGNORE_MSRS == *"Y"* ]] && [[ $IS_EPT == *"Y"* ]]; then
+  echo 'nested kvm virtuallization succesfully enabled '
+else
+  echo 'nested kvm virtualization disabled or works incorrect, please check configuration'
+fi
+
+# activate bash xtrace for script
+[[ "${DEBUG}" == "true" ]] && set -x || set +x
+
+[ -z $ISO_FILE ] && (echo "ISO_FILE variable is empty"; exit 1)
+
+#remove old logs and test data
+[ -f nosetest.xml ] && rm -f nosetests.xml
+rm -rf logs/*
+
+export ISO_PATH="${ISO_STORAGE}/${ISO_FILE}"
+
+if [[ $ISO_FILE == *"Mirantis"* ]]; then
+  export FUEL_RELEASE=$(echo $ISO_FILE | cut -d- -f2 | tr -d '.iso')
+fi
+
+export REQUIRED_FREE_SPACE=200
+
+export ENV_NAME="${ENV_PREFIX}.${CUSTOM_VERSION:10}"
+export VENV_PATH="${HOME}/${FUEL_RELEASE}-venv"
+
+## For plugins we should get a valid version of requrements of python-venv
+## This requirements could be got from the github repo
+## but for each branch of a plugin we should map specific branch of the fuel-qa repo
+## the fuel-qa branch is determined by a fuel-iso name.
+
+case "${FUEL_RELEASE}" in
+  *10* ) export REQS_BRANCH="stable/newton" ;;
+  *90* ) export REQS_BRANCH="stable/mitaka" ;;
+  *80* ) export REQS_BRANCH="stable/8.0"    ;;
+  *70* ) export REQS_BRANCH="stable/7.0"    ;;
+  *61* ) export REQS_BRANCH="stable/6.1"    ;;
+   *   ) export REQS_BRANCH="master"
+esac
+
+REQS_PATH="https://raw.githubusercontent.com/openstack/fuel-qa/${REQS_BRANCH}/fuelweb_test/requirements.txt"
+
+REQS_PATH_DEVOPS="https://raw.githubusercontent.com/openstack/fuel-qa/${REQS_BRANCH}/fuelweb_test/requirements-devops-source.txt"
+
+###############################################################################
+
+## We have limited disk resources, so before run of system tests a lab
+## may have many deployed and runned envs, those may cause errors during test
+
+function dospy {
+  env_list=$2
+  action=$1
+
+  if [[ ! -z "${env_list}" ]] && [[ ! -z "${action}" ]]; then
+    for env in $env_list; do dos.py $action $env; done
+  fi
+}
+
+## Gets dospy environments
+## with prefix the function returns all env except envs like prefix
+
+function dospy_list {
+  prefix=$1
+  dos.py sync
+  [ -z $prefix ] && \
+    echo $(dos.py list | tail -n +3) || \
+    echo $(dos.py list | tail -n +3  | grep -v $prefix)
+}
+
+## Recreate all an virtual env
+function recreate_venv {
+  [ -d $VENV_PATH ] && rm -rf ${VENV_PATH} || echo "The directory ${VENV_PATH} doesn't exist"
+  virtualenv --clear "${VENV_PATH}"
+}
+
+function get_venv_requirements {
+  rm -f requirements.txt*
+  wget $REQS_PATH
+  export REQS_PATH="$(pwd)/requirements.txt"
+  wget $REQS_PATH_DEVOPS
+  export REQS_PATH_DEVOPS="$(pwd)/requirements-devops-source.txt"
+
+  if [[ "${REQS_BRANCH}" == "stable/8.0" ]]; then
+    # bug: https://bugs.launchpad.net/fuel/+bug/1528193
+    sed -i 's/@2.*/@2.9.20/g' $REQS_PATH_DEVOPS
+    #echo oslo.i18n >> $REQS_PATH
+    echo stable/8.0
+  fi
+}
+   
+function prepare_venv {
+    source "${VENV_PATH}/bin/activate"
+    easy_install -U pip
+    if [[ "${DEBUG}" == "true" ]]; then
+        pip install -r "${REQS_PATH}" --upgrade
+        pip install -r "${REQS_PATH_DEVOPS}" --upgrade
+    else
+        pip install -r "${REQS_PATH}" --upgrade > /dev/null 2>/dev/null
+        pip install -r "${REQS_PATH_DEVOPS}" --upgrade > /dev/null 2>/dev/null
+    fi
+
+    django-admin.py syncdb --settings=devops.settings --noinput
+    django-admin.py migrate devops --settings=devops.settings --noinput
+    deactivate
+}
+
+####################################################################################
+
+[[ "${RECREATE_VENV}" == "true" ]] && recreate_venv
+
+get_venv_requirements
+
+[ -d $VENV_PATH ] && prepare_venv || { echo "$VENV_PATH doesn't exist $VENV_PATH will be recreated"; recreate_venv; }
+
+
+source "$VENV_PATH/bin/activate"
+
+[ -z $VIRTUAL_ENV ] && { echo "VIRTUAL_ENV is empty"; exit 1; }
+
+if [[ "${FORCE_ERASE}" -eq "true" ]]; then
+  for env in $(dospy_list $ENV_NAME); do 
+    dos.py erase $env 
+  done 
+else
+
+  # determine free space before run the cleaner
+  free_space=$(df -h | grep '/$' | awk '{print $4}' | tr -d G)
+
+  if (( $free_space < $REQUIRED_FREE_SPACE )); then 
+    for env in $(dospy_list $ENV_NAME); do 
+      dos.py erase $env 
+    done 
+  else
+    echo "free-space: $free_space"
+  fi
+  # poweroff all envs
+  for env in $(dospy_list $ENV_NAME); do 
+    dos.py destroy $env 
+  done 
+fi
--- servers/partner-ci/builders/vcenter.test.ng.sh
+++ servers/partner-ci/builders/vcenter.test.ng.sh
@@ -3,110 +3,112 @@
 # activate bash xtrace for script
 [[ "${DEBUG}" == "true" ]] && set -x || set +x
 [ -z $ISO_FILE  ] && (echo "ISO_FILE variable is empty"; exit 1)
-export SSH_ENDPOINT='ssh_connect.py'
-export ESXI_PASSWORD=${ESXI_PASSWORD:-swordfish}
-export ESXI_USER=${ESXI_USER:-root}
-export ESXI_HOSTS='172.16.0.250 172.16.0.252 172.16.0.253'
-export NFS_SERVER='172.16.0.1'
-export NFS_SHARES='nfs nfs2'
 
-#Set statistics job-group properties for tests
-export FUEL_STATS_HOST=${FUEL_STATS_HOST:-"fuel-collect-systest.infra.mirantis.net"}
-export ANALYTICS_IP="${ANALYTICS_IP:-"fuel-stats-systest.infra.mirantis.net"}"
+#Set statistics job-group properties for swarm tests
+FUEL_STATS_HOST="fuel-collect-systest.infra.mirantis.net"
+ANALYTICS_IP="fuel-stats-systest.infra.mirantis.net"
 
-export MIRROR_HOST=${MIRROR_HOST:-"mirror.seed-cz1.fuel-infra.org"}
+export FUEL_STATS_HOST="${FUEL_STATS_HOST}"
+export ANALYTICS_IP="${ANALYTICS_IP}"
 
-[ ${SNAPSHOTS_ID} ] && export SNAPSHOTS_ID=${SNAPSHOTS_ID} || export SNAPSHOTS_ID=${CUSTOM_VERSION:10}
-[ -z "${SNAPSHOTS_ID}" ] && { echo SNAPSHOTS_ID is empty; exit 1; }
+export MIRROR_HOST="mirror.seed-cz1.fuel-infra.org"
 
-wget --no-check-certificate -O snapshots.params ${SNAPSHOTS_URL/SNAPSHOTS_ID/$SNAPSHOTS_ID}
+if [[ ! "${MIRROR_UBUNTU}" ]]; then
 
-[ -f snapshots.params ] &&  . snapshots.params || \
-  { echo snapshots.params file is not found; exit 1; }
+    case "${UBUNTU_MIRROR_ID}" in
+        latest)
+            UBUNTU_MIRROR_URL="$(curl "http://${MIRROR_HOST}ubuntu-latest.htm")"
+            ;;
+        *)
+            UBUNTU_MIRROR_URL="http://${MIRROR_HOST}/pkgs/${UBUNTU_MIRROR_ID}/"
+    esac
 
-if [[ "${UPDATE_MASTER}" == "true" ]]; then
+    UBUNTU_REPOS="deb ${UBUNTU_MIRROR_URL} trusty main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-updates main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-security main universe multiverse"
 
-  if [[ ! "${MIRROR_UBUNTU}" ]]; then
+    ENABLE_PROPOSED="${ENABLE_PROPOSED:-true}"
 
-      case "${UBUNTU_MIRROR_ID}" in
-          latest)
-              UBUNTU_MIRROR_URL="$(curl "http://${MIRROR_HOST}/pkgs/ubuntu-latest.htm")"
-              ;;
-          *)
-              UBUNTU_MIRROR_URL="http://${MIRROR_HOST}/pkgs/${UBUNTU_MIRROR_ID}/"
-      esac
+    if [ "$ENABLE_PROPOSED" = true ]; then
+        UBUNTU_PROPOSED="deb ${UBUNTU_MIRROR_URL} trusty-proposed main universe multiverse"
+        UBUNTU_REPOS="$UBUNTU_REPOS|$UBUNTU_PROPOSED"
+    fi
 
-      UBUNTU_REPOS="deb ${UBUNTU_MIRROR_URL} trusty main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-updates main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-security main universe multiverse"
+    export MIRROR_UBUNTU="$UBUNTU_REPOS"
 
-      ENABLE_PROPOSED="${ENABLE_PROPOSED:-true}"
-
-      if [ "$ENABLE_PROPOSED" = true ]; then
-          UBUNTU_PROPOSED="deb ${UBUNTU_MIRROR_URL} trusty-proposed main universe multiverse"
-          UBUNTU_REPOS="$UBUNTU_REPOS|$UBUNTU_PROPOSED"
-      fi
-
-      export MIRROR_UBUNTU="$UBUNTU_REPOS"
-
-  fi
-
-  function join() {
-      local __sep="${1}"
-      local __head="${2}"
-      local __tail="${3}"
-      [[ -n "${__head}" ]] && echo "${__head}${__sep}${__tail}" || echo "${__tail}"
-  }
-
-  function to_uppercase() {
-      echo "$1" | awk '{print toupper($0)}'
-  }
-
-  __space=' '
-  __pipe='|'
-
-  # Adding MOS rpm repos to
-  # - UPDATE_FUEL_MIRROR - will be used for master node
-  # - EXTRA_RPM_REPOS - will be used for nodes in cluster
-  for _dn in  "os"        \
-              "proposed"  \
-              "updates"   \
-              "holdback"  \
-              "hotfix"    \
-              "security"  ; do
-      # a pointer to variable name which holds value of enable flag for this dist name
-      __enable_ptr="ENABLE_MOS_CENTOS_$(to_uppercase "${_dn}")"
-      if [[ "${!__enable_ptr}" = true ]] ; then
-          # a pointer to variable name which holds repo id
-          __repo_id_ptr="MOS_CENTOS_$(to_uppercase "${_dn}")_MIRROR_ID"
-          __repo_url="http://${MIRROR_HOST}/mos-repos/centos/mos9.0-centos7/snapshots/${!__repo_id_ptr}/x86_64"
-          __repo_name="mos-${_dn},${__repo_url}"
-          UPDATE_FUEL_MIRROR="$(join "${__space}" "${UPDATE_FUEL_MIRROR}" "${__repo_url}" )"
-          EXTRA_RPM_REPOS="$(join "${__pipe}" "${EXTRA_RPM_REPOS}" "${__repo_name}" )"
-      fi
-  done
-
-  # Adding MOS deb repos to
-  # - EXTRA_DEB_REPOS - will be used for nodes in cluster
-  for _dn in  "proposed"  \
-              "updates"   \
-              "holdback"  \
-              "hotfix"    \
-              "security"  ; do
-      # a pointer to variable name which holds value of enable flag for this dist name
-      __enable_ptr="ENABLE_MOS_UBUNTU_$(to_uppercase "${_dn}")"
-      # a pointer to variable name which holds repo id
-      __repo_id_ptr="MOS_UBUNTU_MIRROR_ID"
-      __repo_url="http://${MIRROR_HOST}/mos-repos/ubuntu/snapshots/${!__repo_id_ptr}"
-      if [[ "${!__enable_ptr}" = true ]] ; then
-          __repo_name="mos-${_dn},deb ${__repo_url} mos9.0-${_dn} main restricted"
-          EXTRA_DEB_REPOS="$(join "${__pipe}" "${EXTRA_DEB_REPOS}" "${__repo_name}")"
-      fi
-  done
-
-  export UPDATE_FUEL_MIRROR   # for fuel-qa
-  export UPDATE_MASTER        # for fuel-qa
-  export EXTRA_RPM_REPOS      # for fuel-qa
-  export EXTRA_DEB_REPOS      # for fuel-qa
 fi
+
+function join() {
+    local __sep="${1}"
+    local __head="${2}"
+    local __tail="${3}"
+
+    if [[ -n "${__head}" ]]; then
+        echo "${__head}${__sep}${__tail}"
+    else
+        echo "${__tail}"
+    fi
+}
+
+function to_uppercase() {
+    echo "$1" | awk '{print toupper($0)}'
+}
+
+__space=' '
+__pipe='|'
+
+
+# Adding MOS rpm repos to
+# - UPDATE_FUEL_MIRROR - will be used for master node
+# - EXTRA_RPM_REPOS - will be used for nodes in cluster
+
+for _dn in  "os"        \
+            "proposed"  \
+            "updates"   \
+            "holdback"  \
+            "hotfix"    \
+            "security"  ; do
+    # a pointer to variable name which holds value of enable flag for this dist name
+    __enable_ptr="ENABLE_MOS_CENTOS_$(to_uppercase "${_dn}")"
+    if [[ "${!__enable_ptr}" = true ]] ; then
+        # a pointer to variable name which holds repo id
+        __repo_id_ptr="MOS_CENTOS_$(to_uppercase "${_dn}")_MIRROR_ID"
+        __repo_url="http://${MIRROR_HOST}/mos-repos/centos/mos9.0-centos7/snapshots/${!__repo_id_ptr}/x86_64"
+        __repo_name="mos-${_dn},${__repo_url}"
+        UPDATE_FUEL_MIRROR="$(join "${__space}" "${UPDATE_FUEL_MIRROR}" "${__repo_url}" )"
+        EXTRA_RPM_REPOS="$(join "${__pipe}" "${EXTRA_RPM_REPOS}" "${__repo_name}" )"
+    fi
+done
+
+# UPDATE_MASTER=true in case when we have set some repos
+# otherwise there will be no reason to start updating without any repos to update from
+
+if [[ -n "${UPDATE_FUEL_MIRROR}" ]] ; then
+    UPDATE_MASTER=true
+fi
+
+# Adding MOS deb repos to
+# - EXTRA_DEB_REPOS - will be used for nodes in cluster
+
+for _dn in  "proposed"  \
+            "updates"   \
+            "holdback"  \
+            "hotfix"    \
+            "security"  ; do
+    # a pointer to variable name which holds value of enable flag for this dist name
+    __enable_ptr="ENABLE_MOS_UBUNTU_$(to_uppercase "${_dn}")"
+    # a pointer to variable name which holds repo id
+    __repo_id_ptr="MOS_UBUNTU_MIRROR_ID"
+    __repo_url="http://${MIRROR_HOST}/mos-repos/ubuntu/snapshots/${!__repo_id_ptr}"
+    if [[ "${!__enable_ptr}" = true ]] ; then
+        __repo_name="mos-${_dn},deb ${__repo_url} mos9.0-updates main restricted"
+        EXTRA_DEB_REPOS="$(join "${__pipe}" "${EXTRA_DEB_REPOS}" "${__repo_name}")"
+    fi
+done
+
+
+export UPDATE_FUEL_MIRROR   # for fuel-qa
+export UPDATE_MASTER        # for fuel-qa
+export EXTRA_RPM_REPOS      # for fuel-qa
+export EXTRA_DEB_REPOS      # for fuel-qa
 
 PLUGIN_VERSION_ARTIFACT='build.plugin_version'
 
@@ -137,7 +139,7 @@ if [[ $ISO_FILE == *"Mirantis"* ]]; then
   export FUEL_RELEASE=$(echo $ISO_FILE | cut -d- -f2 | tr -d '.iso')
 fi
 
-export ENV_NAME="${ENV_PREFIX}.${SNAPSHOTS_ID}"
+export ENV_NAME="${ENV_PREFIX}.${CUSTOM_VERSION:10}"
 export VENV_PATH="${HOME}/${FUEL_RELEASE}-venv"
 
 [ -z "${DVS_PLUGIN_PATH}" ] && export DVS_PLUGIN_PATH=$(ls -t ${WORKSPACE}/fuel-plugin-vmware-dvs*.rpm | head -n 1)
@@ -161,18 +163,8 @@ echo "iso-path: ${ISO_PATH}"
 echo "plugin-path: ${DVS_PLUGIN_PATH}"
 echo "plugin-checksum: $(md5sum -b ${DVS_PLUGIN_PATH})"
 
-cat << UPDATE_PROPERTIES > update.properties
-SNAPSHOTS_ID=$SNAPSHOTS_ID
-UPDATE_FUEL_MIRROR=$UPDATE_FUEL_MIRROR
-UPDATE_MASTER=$UPDATE_MASTER
-EXTRA_RPM_REPOS=$EXTRA_RPM_REPOS
-EXTRA_DEB_REPOS=$EXTRA_DEB_REPOS
-UPDATE_PROPERTIES
-
-[[ "${UPDATE_MASTER}" == "true" ]] && cat snapshots.params >> update.properties
-
 cat << REPORTER_PROPERTIES > reporter.properties
-ISO_VERSION=${SNAPSHOTS_ID}
+ISO_VERSION=${CUSTOM_VERSION:10}
 ISO_FILE=$ISO_FILE
 TEST_GROUP=$TEST_GROUP
 TEST_GROUP_CONFIG=$TEST_GROUP_CONFIG
@@ -195,190 +187,7 @@ source "${VENV_PATH}/bin/activate"
 #                           -t "${TEST_GROUP_PREFIX}(${TEST_GROUP_CONFIG})" \
 #                           $systest_parameters
 
-#/btsync/tpi_systest_mod2.sh -d ${OPENSTACK_RELEASE} \
-#                            -i ${ISO_PATH} \
-#                            -t "${TEST_GROUP_PREFIX}(${TEST_GROUP_CONFIG})" \
-#                            $systest_parameters
-
-main() {
-
-  #clean_old_bridges
-
-  rm -rf logs/*
-  if [ -z $NOREVERT ]; then
-    revert_ws "$WORKSTATION_NODES" $SYSTEST_PID
-    restart_ws_network
-    echo "waiting for vsphere vms netwokring"
-    sleep 10
-    configure_nfs
-  fi
-
-  #Run test in background and wait before environment is created
-  echo sh -x "utils/jenkins/system_tests.sh" -t test $systest_parameters -i "${ISO_PATH}" -o --group=$TEST_GROUP
-  sh -x "utils/jenkins/system_tests.sh" \
-      -t test $systest_parameters \
-      -i "${ISO_PATH}" \
-      -o --group="${TEST_GROUP_PREFIX}(${TEST_GROUP_CONFIG})" &
-
-  export SYSTEST_PID=$!
-
-  #Wait before environment is created
-  while [ $(virsh net-list | grep $ENV_NAME | wc -l) -ne 5 ]; do
-    sleep 10
-    if ! ps -p $SYSTEST_PID > /dev/null
-    then
-      echo System tests exited prematurely, aborting
-      exit 1
-    fi
-  done
-  sleep 5
-
-  add_interface_to_bridge $ENV_NAME private vmnet2
-
-  clean_iptables
-
-  echo waiting for system tests to finish
-  wait $SYSTEST_PID
-  export RES=$?
-  echo ENVIRONMENT NAME is $ENV_NAME
-
-  virsh net-dumpxml "${ENV_NAME}_admin" | grep -P "(\d+\.){3}" -o | awk '{print "Fuel master node IP: "$0"2"}'
-
-  echo Result is $RES
-
-  if [ $RES -eq 0 ]; then
-    echo Tests succeeded
-    if [ -n  "$ERASE_ENV_AFTER" ]; then
-      echo Erasing $ENV_NAME
-      dos.py erase $ENV_NAME
-    fi
-    if [ -n "$REVERT_AFTER" ]; then
-      revert_ws "$WORKSTATION_NODES" $SYSTEST_PID
-    fi
-  else
-    [[ $DESTROY_ENV_AFTER == "true"  ]] && dos.py destroy $ENV_NAME
-    echo Tests failed
-  fi
-
-}
-
-kill_test(){
-    pid=$1
-    if [ ! -z $pid ]; then
-        echo "killing $pid and its childs" && pkill --parent $pid && kill $pid && exit 1;
-
-    elif [ -z $SYSTEST_PID ]; then
-        echo "killing $pid and its childs" && pkill --parent $pid && kill $pid && exit 1;
-
-    else
-        echo "test process id doesn't exist"
-        exit 1
-    fi
-}
-
-add_interface_to_bridge() {
-  env=$1
-  net_name=$2
-  nic=$3
-  ip=$4
-
-  for net in $(virsh net-list | grep ${env}_${net_name} | awk '{print $1}'); do
-    bridge=$(virsh net-info $net | grep -i bridge |awk '{print $2}')
-    setup_bridge $bridge $nic $ip && echo $net_name bridge $bridge ready
-  done
-}
-
-setup_bridge() {
-  bridge=$1
-  nic=$2
-
-  sudo brctl stp $bridge off
-  sudo brctl addif $bridge $nic
-
-  sudo ip address flush $nic
-  sudo ip link set dev $nic up
-  sudo ip link set dev $bridge up
-
-  if sudo /sbin/iptables-save | grep $bridge | grep -i reject | grep -q FORWARD; then
-    sudo /sbin/iptables -D FORWARD -o $bridge -j REJECT --reject-with icmp-port-unreachable
-    sudo /sbin/iptables -D FORWARD -i $bridge -j REJECT --reject-with icmp-port-unreachable
-  fi
-}
-
-clean_iptables() {
-  sudo /sbin/iptables -F
-  sudo /sbin/iptables -t nat -F
-  sudo /sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-}
-
-restart_ws_network(){
-  sudo vmware-networks --stop
-  sudo vmware-networks --start
-}
-
-revert_ws() {
-  set +x
-  systest_pid=$2
-  cmd="vmrun -T ws-shared -h https://localhost:443/sdk -u ${WORKSTATION_USERNAME} -p ${WORKSTATION_PASSWORD}"
-  for i in $1; do
-    $cmd listRegisteredVM | grep -q $i || { echo "VM $i does not exist"; kill_test $systest_pid; }
-    echo vmrun: reverting $i to $WORKSTATION_SNAPSHOT
-    $cmd revertToSnapshot "[standard] $i/$i.vmx" $WORKSTATION_SNAPSHOT && echo "vmrun: $i reverted" || { echo "Error: revert of $i failed";  kill_test $systest_pid; }
-    echo vmrun: starting $i
-    $cmd start "[standard] $i/$i.vmx" && echo "vmrun: $i is started" || { echo "Error: $i failed to start";  kill_test $systest_pid; }
-  done
-  set -x
-}
-
-create_ssh_endpoint(){
-
-cat << SSH_ENDPOINT > $SSH_ENDPOINT
-#!/usr/bin/python2
-import paramiko
-import sys
-
-host = sys.argv[1]
-user = sys.argv[2]
-secret = sys.argv[3]
-command = sys.argv[4]
-port = 22
-
-client = paramiko.SSHClient()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect(hostname=host, username=user, password=secret, port=port)
-stdin, stdout, stderr = client.exec_command(command)
-data = stdout.read() + stderr.read()
-print data
-client.close()
-SSH_ENDPOINT
-
-chmod +x $SSH_ENDPOINT
-
-}
-
-configure_nfs(){
-  create_ssh_endpoint
-
-  for esxi_host in $ESXI_HOSTS; do
-
-    python2 $SSH_ENDPOINT $esxi_host $ESXI_USER $ESXI_PASSWORD 'storages=$(esxcli storage nfs list | grep nfs | cut -d" " -f1); for storage in $storages; do esxcli storage nfs remove -v $storage; done'
-    echo "nfs storages have been successfully removed for $esxi_host"
-
-    for nfs_share in $NFS_SHARES; do
-      python2 $SSH_ENDPOINT $esxi_host $ESXI_USER $ESXI_PASSWORD "esxcli storage nfs add -H $NFS_SERVER -s /var/$nfs_share -v $nfs_share"
-      echo "$nfs_share has been successfully connected for $esxi_host"
-    done
-
-    python2 $SSH_ENDPOINT $esxi_host $ESXI_USER $ESXI_PASSWORD 'esxcli storage core adapter rescan --all'
-    echo "Rescan all datastores for $esxi_host"
-
-  done
-}
-
-check_testgroup() {
-    run_system_tests.sh explain --group=$1
-}
-
-main
-
-exit $RES
+/btsync/tpi_systest_mod2.sh -d ${OPENSTACK_RELEASE} \
+                            -i ${ISO_PATH} \
+                            -t "${TEST_GROUP_PREFIX}(${TEST_GROUP_CONFIG})" \
+                            $systest_parameters
