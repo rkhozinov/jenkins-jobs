51264f7ac700fd81c0f84337414877eb90d4067b
Ruslan Khozinov <ruslan.khozinov@gmail.com>
2016-07-27 16:32:25 +0300

    9.1.vcenter
     - update to snapshots only if update_master is true

--- servers/partner-ci/builders/vcenter.test.ng.sh
+++ servers/partner-ci/builders/vcenter.test.ng.sh
@@ -19,102 +19,89 @@ wget --no-check-certificate -O snapshots.params ${SNAPSHOTS_URL/SNAPSHOTS_ID/$SN
 [ -f snapshots.params ] &&  . snapshots.params || \
   { echo snapshots.params file is not found; exit 1; }
 
-if [[ ! "${MIRROR_UBUNTU}" ]]; then
+if [[ "${UPDATE_MASTER}" == "true" ]]; then
 
-    case "${UBUNTU_MIRROR_ID}" in
-        latest)
-            UBUNTU_MIRROR_URL="$(curl "http://${MIRROR_HOST}/pkgs/ubuntu-latest.htm")"
-            ;;
-        *)
-            UBUNTU_MIRROR_URL="http://${MIRROR_HOST}/pkgs/${UBUNTU_MIRROR_ID}/"
-    esac
-
-    UBUNTU_REPOS="deb ${UBUNTU_MIRROR_URL} trusty main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-updates main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-security main universe multiverse"
-
-    ENABLE_PROPOSED="${ENABLE_PROPOSED:-true}"
-
-    if [ "$ENABLE_PROPOSED" = true ]; then
-        UBUNTU_PROPOSED="deb ${UBUNTU_MIRROR_URL} trusty-proposed main universe multiverse"
-        UBUNTU_REPOS="$UBUNTU_REPOS|$UBUNTU_PROPOSED"
-    fi
-
-    export MIRROR_UBUNTU="$UBUNTU_REPOS"
-
-fi
-
-function join() {
-    local __sep="${1}"
-    local __head="${2}"
-    local __tail="${3}"
-
-    if [[ -n "${__head}" ]]; then
-        echo "${__head}${__sep}${__tail}"
-    else
-        echo "${__tail}"
-    fi
-}
-
-function to_uppercase() {
-    echo "$1" | awk '{print toupper($0)}'
-}
-
-__space=' '
-__pipe='|'
-
-
-# Adding MOS rpm repos to
-# - UPDATE_FUEL_MIRROR - will be used for master node
-# - EXTRA_RPM_REPOS - will be used for nodes in cluster
-
-for _dn in  "os"        \
-            "proposed"  \
-            "updates"   \
-            "holdback"  \
-            "hotfix"    \
-            "security"  ; do
-    # a pointer to variable name which holds value of enable flag for this dist name
-    __enable_ptr="ENABLE_MOS_CENTOS_$(to_uppercase "${_dn}")"
-    if [[ "${!__enable_ptr}" = true ]] ; then
-        # a pointer to variable name which holds repo id
-        __repo_id_ptr="MOS_CENTOS_$(to_uppercase "${_dn}")_MIRROR_ID"
-        __repo_url="http://${MIRROR_HOST}/mos-repos/centos/mos9.0-centos7/snapshots/${!__repo_id_ptr}/x86_64"
-        __repo_name="mos-${_dn},${__repo_url}"
-        UPDATE_FUEL_MIRROR="$(join "${__space}" "${UPDATE_FUEL_MIRROR}" "${__repo_url}" )"
-        EXTRA_RPM_REPOS="$(join "${__pipe}" "${EXTRA_RPM_REPOS}" "${__repo_name}" )"
-    fi
-done
-
-# UPDATE_MASTER=true in case when we have set some repos
-# otherwise there will be no reason to start updating without any repos to update from
-
-if [[ -n "${UPDATE_FUEL_MIRROR}" ]] ; then
-    UPDATE_MASTER=true
-fi
-
-# Adding MOS deb repos to
-# - EXTRA_DEB_REPOS - will be used for nodes in cluster
-
-for _dn in  "proposed"  \
-            "updates"   \
-            "holdback"  \
-            "hotfix"    \
-            "security"  ; do
-    # a pointer to variable name which holds value of enable flag for this dist name
-    __enable_ptr="ENABLE_MOS_UBUNTU_$(to_uppercase "${_dn}")"
-    # a pointer to variable name which holds repo id
-    __repo_id_ptr="MOS_UBUNTU_MIRROR_ID"
-    __repo_url="http://${MIRROR_HOST}/mos-repos/ubuntu/snapshots/${!__repo_id_ptr}"
-    if [[ "${!__enable_ptr}" = true ]] ; then
-        __repo_name="mos-${_dn},deb ${__repo_url} mos9.0-${_dn} main restricted"
-        EXTRA_DEB_REPOS="$(join "${__pipe}" "${EXTRA_DEB_REPOS}" "${__repo_name}")"
-    fi
-done
-
-
-export UPDATE_FUEL_MIRROR   # for fuel-qa
-export UPDATE_MASTER        # for fuel-qa
-export EXTRA_RPM_REPOS      # for fuel-qa
-export EXTRA_DEB_REPOS      # for fuel-qa
+  if [[ ! "${MIRROR_UBUNTU}" ]]; then
+  
+      case "${UBUNTU_MIRROR_ID}" in
+          latest)
+              UBUNTU_MIRROR_URL="$(curl "http://${MIRROR_HOST}/pkgs/ubuntu-latest.htm")"
+              ;;
+          *)
+              UBUNTU_MIRROR_URL="http://${MIRROR_HOST}/pkgs/${UBUNTU_MIRROR_ID}/"
+      esac
+  
+      UBUNTU_REPOS="deb ${UBUNTU_MIRROR_URL} trusty main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-updates main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-security main universe multiverse"
+  
+      ENABLE_PROPOSED="${ENABLE_PROPOSED:-true}"
+  
+      if [ "$ENABLE_PROPOSED" = true ]; then
+          UBUNTU_PROPOSED="deb ${UBUNTU_MIRROR_URL} trusty-proposed main universe multiverse"
+          UBUNTU_REPOS="$UBUNTU_REPOS|$UBUNTU_PROPOSED"
+      fi
+  
+      export MIRROR_UBUNTU="$UBUNTU_REPOS"
+  
+  fi
+  
+  function join() {
+      local __sep="${1}"
+      local __head="${2}"
+      local __tail="${3}"
+      [[ -n "${__head}" ]] && echo "${__head}${__sep}${__tail}" || echo "${__tail}"
+  }
+  
+  function to_uppercase() {
+      echo "$1" | awk '{print toupper($0)}'
+  }
+  
+  __space=' '
+  __pipe='|'
+  
+  # Adding MOS rpm repos to
+  # - UPDATE_FUEL_MIRROR - will be used for master node
+  # - EXTRA_RPM_REPOS - will be used for nodes in cluster
+  for _dn in  "os"        \
+              "proposed"  \
+              "updates"   \
+              "holdback"  \
+              "hotfix"    \
+              "security"  ; do
+      # a pointer to variable name which holds value of enable flag for this dist name
+      __enable_ptr="ENABLE_MOS_CENTOS_$(to_uppercase "${_dn}")"
+      if [[ "${!__enable_ptr}" = true ]] ; then
+          # a pointer to variable name which holds repo id
+          __repo_id_ptr="MOS_CENTOS_$(to_uppercase "${_dn}")_MIRROR_ID"
+          __repo_url="http://${MIRROR_HOST}/mos-repos/centos/mos9.0-centos7/snapshots/${!__repo_id_ptr}/x86_64"
+          __repo_name="mos-${_dn},${__repo_url}"
+          UPDATE_FUEL_MIRROR="$(join "${__space}" "${UPDATE_FUEL_MIRROR}" "${__repo_url}" )"
+          EXTRA_RPM_REPOS="$(join "${__pipe}" "${EXTRA_RPM_REPOS}" "${__repo_name}" )"
+      fi
+  done
+  
+  # Adding MOS deb repos to
+  # - EXTRA_DEB_REPOS - will be used for nodes in cluster
+  for _dn in  "proposed"  \
+              "updates"   \
+              "holdback"  \
+              "hotfix"    \
+              "security"  ; do
+      # a pointer to variable name which holds value of enable flag for this dist name
+      __enable_ptr="ENABLE_MOS_UBUNTU_$(to_uppercase "${_dn}")"
+      # a pointer to variable name which holds repo id
+      __repo_id_ptr="MOS_UBUNTU_MIRROR_ID"
+      __repo_url="http://${MIRROR_HOST}/mos-repos/ubuntu/snapshots/${!__repo_id_ptr}"
+      if [[ "${!__enable_ptr}" = true ]] ; then
+          __repo_name="mos-${_dn},deb ${__repo_url} mos9.0-${_dn} main restricted"
+          EXTRA_DEB_REPOS="$(join "${__pipe}" "${EXTRA_DEB_REPOS}" "${__repo_name}")"
+      fi
+  done
+  
+  export UPDATE_FUEL_MIRROR   # for fuel-qa
+  export UPDATE_MASTER        # for fuel-qa
+  export EXTRA_RPM_REPOS      # for fuel-qa
+  export EXTRA_DEB_REPOS      # for fuel-qa
+fi 
 
 PLUGIN_VERSION_ARTIFACT='build.plugin_version'
 
