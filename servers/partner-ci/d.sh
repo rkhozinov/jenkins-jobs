c915f17d7a43dfa969cccae709a4050a95e759ef
Ruslan Khozinov <ruslan.khozinov@gmail.com>
2016-08-11 17:32:15 +0300

    vcenter9.1
     - vmnet3 and vmnet2 should be added simultaneously to the private
       fuel bridge, because dvs in vsphere is configured to use
       2 active uplinks
     - vcenter test confiugration should use separate dvs per cluster

--- servers/partner-ci/d.sh
+++
@@ -1,9 +0,0 @@
-MIRROR_HOST=''
-echo $MIRROR_HOST
-export MIRROR_HOST=${MIRROR_HOST:="mirror.seed-cz1.fuel-infra.org"}
-echo $MIRROR_HOST
-
-MIRROR_HOST=''
-export MIRROR_HOST=${MIRROR_HOST:-"mirror.seed-cz1.fuel-infra.org"}
-echo $MIRROR_HOST
-
