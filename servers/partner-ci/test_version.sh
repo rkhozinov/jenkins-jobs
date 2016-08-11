d6edd4f7ec1501dc93fe90ddb160926b1000a7e0
Ruslan Khozinov <ruslan.khozinov@gmail.com>
2016-08-11 17:32:15 +0300

    vcenter9.1
     - vmnet3 and vmnet2 should be added simultaneously to the private
       fuel bridge, because dvs in vsphere is configured to use
       2 active uplinks
     - vcenter test confiugration should use separate dvs per cluster

--- servers/partner-ci/test_version.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ISO_FILE="fuel-9.0-mos-364.iso"
-#ISO_FILE="fuel-9.0-mos-370-2016-05-18_06-18-59.iso"
-[[ "$ISO_FILE" == *mos* ]] && echo "${ISO_FILE}" | cut -d'-' -f4-4 | tr -d '.iso' || \
-                              echo "${ISO_FILE}" | cut -d'-' -f3-3 | tr -d '.iso'
