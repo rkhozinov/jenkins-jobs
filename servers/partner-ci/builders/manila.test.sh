ca71217190bc19fad1845cc8cd26b790a69acee2
Ruslan Khozinov <ruslan.khozinov@gmail.com>
2016-08-16 15:54:21 +0300

    9.0/manila: some changes
    - nightly is added
    - smoke is added
    - bvt is added
    - label is changed
    - test script is fixed

--- servers/partner-ci/builders/manila.test.sh
+++ servers/partner-ci/builders/manila.test.sh
@@ -1,52 +1,25 @@
 #!/bin/bash -e
+# activate bash xtrace for script
 [[ "${DEBUG}" == "true" ]] && set -x || set +x
 
-export ISO_PATH="${ISO_STORAGE}/${ISO_FILE}"
-[ -z $ISO_PATH  ] && { echo "ISO_PATH is empty or doesn't exist"; exit 1; }
-
-if [[ $ISO_FILE == *"custom"* ]]; then
-  export FUEL_RELEASE=90
-else
-  export FUEL_RELEASE=$(echo $ISO_FILE | cut -d- -f2 | tr -d '.iso')
+ISO_FILE_ARTIFACT='iso_file'
+# if user entered custom iso we should use it
+if [ -z $ISO_FILE  ]; then
+   # but if use doesn't want custom iso, we should get iso from artifacts
+   [ -f $ISO_FILE_ARTIFACT ] && source $ISO_FILE_ARTIFACT || (echo "There's not iso_file"; exit 1)
+   # check variable again - it shouldn't be empty
+   [ -z $ISO_FILE  ] && (echo "ISO_FILE variable is empty"; exit 1)
 fi
 
-if [ "${SNAPSHOTS_ID}" != "released" ]; then
-  if [[ "${UPDATE_MASTER}" == "true" ]] && [[ ${FUEL_RELEASE} != *"80"* ]]; then
-    if [ -f $SNAPSHOT_OUTPUT_FILE ]; then
-      . $SNAPSHOT_OUTPUT_FILE
-      export EXTRA_RPM_REPOS
-      export UPDATE_FUEL_MIRROR
-      export EXTRA_DEB_REPOS
-    else
-      echo "SNAPSHOT_OUTPUT_FILE is empty or doesn't exist"
-      exit 1
-    fi
-  else
-    export SNAPSHOTS_ID="released"
-  fi
+PLUGIN_VERSION_ARTIFACT='build.plugin_version'
+# if user entered custom iso we should use it
+
+if [ -z $PLUGIN_VERSION  ]; then
+   # but if use doesn't want custom iso, we should get iso from artifacts
+   [ -f $PLUGIN_VERSION_ARTIFACT ] && source $PLUGIN_VERSION_ARTIFACT || (echo "The PLUGIN_VERSION is empty"; exit 1)
 fi
 
-if [[ $SNAPSHOTS_ID == *"lastSuccessfulBuild"* ]]; then
-  export SNAPSHOTS_ID=$(cat snapshots.params | grep -Po '#\K[^ ]+')
-fi
-
-[ -z "${SNAPSHOTS_ID}" ] && { echo SNAPSHOTS_ID is empty; exit 1; }
-
-if [ -f build.plugin_version ]; then
-  export MANILA_PLUGIN_VERSION=$(grep "PLUGIN_VERSION" < build.plugin_version | cut -d= -f2 )
-else
-  if [ -z $PLUGIN_VERSION ]; then
-    echo "build.properties file is not available so a test couldn't be runned"
-    exit 1
-  else
-    export MANILA_PLUGIN_VERSION=$PLUGIN_VERSION
-  fi
-fi
-
-[ -z $MANILA_PLUGIN_VERSION ] && { echo "MANILA_PLUGIN_VERSION is empty"; exit 1; }
-
-[ ! -f $MANILA_IMAGE_PATH ] && \
-  { echo "MANILA_IMAGE_PATH is empty or doesn't exist"; exit 1; }
+[ -z $PLUGIN_VERSION  ] && { echo "PLUGIN_VERSION variable is empty"; exit 1; } || export MANILA_PLUGIN_VERSION=$PLUGIN_VERSION
 
 if [ -z "${PKG_JOB_BUILD_NUMBER}" ]; then
     if [ -f build.properties ]; then
@@ -59,23 +32,25 @@ if [ -z "${PKG_JOB_BUILD_NUMBER}" ]; then
 fi
 
 #remove old logs and test data
-[ -f nosetest.xml ] && sudo rm -f nosetests.xml
-sudo rm -rf logs/*
+[ -f nosetest.xml ] && rm -f nosetests.xml
+rm -rf logs/*
 
+export ISO_PATH="${ISO_STORAGE}/${ISO_FILE}"
 if [[ $ISO_FILE == *"Mirantis"* ]]; then
-  export FUEL_RELEASE=$(echo $ISO_FILE | cut -d- -f2 | tr -d '.iso')
+  export ISO_VERSION=$(echo $ISO_FILE | tr -d '.iso' )
+  export FUEL_RELEASE=$(echo $ISO_VERSION | cut -d- -f2)
 fi
-
-if [[ $ISO_FILE == *"custom"* ]]; then
-  export FUEL_RELEASE=90
-fi
-
-export ENV_NAME="${ENV_PREFIX}.${SNAPSHOTS_ID}"
+    
+export ENV_NAME="${ENV_PREFIX}.${ISO_VERSION}"
 export VENV_PATH="${HOME}/${FUEL_RELEASE}-venv"
 
 [ -z "${MANILA_PLUGIN_PATH}" ] && export MANILA_PLUGIN_PATH=$(ls -t ${WORKSPACE}/fuel-plugin-manila*.rpm | head -n 1)
 [ -z "${MANILA_PLUGIN_PATH}" ] && { echo "MANILA_PLUGIN_PATH is empty"; exit 1; }
 [ -z "${PLUGIN_PATH}"     ] && export PLUGIN_PATH=$MANILA_PLUGIN_PATH
+
+systest_parameters=''
+[[ $USE_SNAPSHOTS == "true"  ]] && systest_parameters+=' -k' || echo "new env will be created"
+[[ $ERASE_AFTER   == "true"  ]] && echo "the env will be erased after test" || systest_parameters+=' -K'
 
 echo "test-group: ${TEST_GROUP}"
 echo "env-name: ${ENV_NAME}"
@@ -84,20 +59,18 @@ echo "fuel-release: ${FUEL_RELEASE}"
 echo "venv-path: ${VENV_PATH}"
 echo "env-name: ${ENV_NAME}"
 echo "iso-path: ${ISO_PATH}"
-echo "plugin-path: ${MANILA_PLUGIN_PATH}"
-echo "plugin-checksum: $(md5sum -b ${MANILA_PLUGIN_PATH})"
+echo "plugin-path: ${DVS_PLUGIN_PATH}"
+echo "plugin-checksum: $(md5sum -b ${DVS_PLUGIN_PATH})"
 
 cat << REPORTER_PROPERTIES > reporter.properties
-ISO_VERSION=$SNAPSHOTS_ID
-SNAPSHOTS_ID=$SNAPSHOTS_ID
+ISO_VERSION=$ISO_VERSION
 ISO_FILE=$ISO_FILE
 TEST_GROUP=$TEST_GROUP
 TEST_JOB_NAME=$JOB_NAME
 TEST_JOB_BUILD_NUMBER=$BUILD_NUMBER
 PKG_JOB_BUILD_NUMBER=$PKG_JOB_BUILD_NUMBER
 PLUGIN_VERSION=$PLUGIN_VERSION
-MANILA_PLUGIN_VERSION=$MANILA_PLUGIN_VERSION
-TREP_TESTRAIL_SUITE=$TREP_TESTRAIL_SUITE
+TREP_TESTRAIL_SUITE=${TREP_TESTRAIL_SUITE}
 TREP_TESTRAIL_SUITE_DESCRIPTION=$TREP_TESTRAIL_SUITE_DESCRIPTION
 TREP_TESTRAIL_PLAN=$TREP_TESTRAIL_PLAN
 TREP_TESTRAIL_PLAN_DESCRIPTION=$TREP_TESTRAIL_PLAN_DESCRIPTION
@@ -106,80 +79,4 @@ REPORTER_PROPERTIES
 
 source "${VENV_PATH}/bin/activate"
 
-add_interface_to_bridge() {
-  env=$1
-  net_name=$2
-  nic=$3
-
-  for net in $(virsh net-list | grep ${env}_${net_name} | awk '{print $1}'); do
-    bridge=$(virsh net-info $net | grep -i bridge |awk '{print $2}')
-    setup_bridge $bridge $nic && echo $net_name bridge $bridge ready
-  done
-}
-
-setup_bridge() {
-  set -x
-  bridge=$1
-  nic=$2
-
-  [[ "${DISABLE_STP}" == "true" ]] && sudo brctl stp $bridge off
-  sudo brctl addif $bridge $nic
-
-  sudo ip address flush $nic
-  sudo ip link set dev $nic up
-  sudo ip link set dev $bridge up
-
-  if sudo /sbin/iptables-save | grep $bridge | grep -i reject | grep -q FORWARD; then
-    sudo /sbin/iptables -D FORWARD -o $bridge -j REJECT --reject-with icmp-port-unreachable
-    sudo /sbin/iptables -D FORWARD -i $bridge -j REJECT --reject-with icmp-port-unreachable
-  fi
-
-  if [[ "${DEBUG}" == "true" ]]; then
-    sudo brctl show $bridge
-    sudo ip address show $bridge
-    sudo ip address show $nic
-  fi
-}
-
-clean_iptables() {
-  sudo /sbin/iptables -F
-  sudo /sbin/iptables -t nat -F
-  sudo /sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-}
-
-# run python test set to create environments, deploy and test product
-# WORKAROUND for https://bugs.launchpad.net/fuel/+bug/1642991
-export WORKSPACE="${WORKSPACE}/plugin_test/fuel-qa"
-export PLUGIN_WORKSPACE="${WORKSPACE/\/fuel-qa}"
-export PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}${WORKSPACE}:${PLUGIN_WORKSPACE}"
-[[ "${DEBUG}" == "true" ]] && echo "PYTHONPATH:${PYTHONPATH} PATH:${PATH} WORKSPACE:${WORKSPACE}"
-python $PLUGIN_WORKSPACE/run_tests.py -q --nologcapture --with-xunit --group=${TEST_GROUP} &
-export SYSTEST_PID=$!
-
-#Wait before environment is created
-while [ true ]; do
-  [ $(virsh net-list | grep $ENV_NAME | wc -l) -eq 5 ] && break || sleep 10
-  [ -e /proc/$SYSTEST_PID ] && continue || \
-    { echo System tests exited prematurely, aborting; exit 1; }
-done
-
-[[ "${CLEAN_IPTABLES}" == "true" ]] && clean_iptables
-
-for iface_map in $(echo $WORKSTATION_IFS | tr ',' ' '); do
-    iface=$(echo $iface_map | cut -d: -f1)
-    network=$(echo $iface_map | cut -d: -f2)
-    add_interface_to_bridge $ENV_NAME $network $iface
-done
-
-[[ "${DEBUG}" == "true" ]] && \
-    sudo iptables -L -v -n;   \
-    sudo iptables -t nat -L -v -n
-
-echo "Waiting for system tests to finish"
-wait $SYSTEST_PID
-export RESULT=$?
-
-echo "ENVIRONMENT NAME is $ENV_NAME"
-dos.py list --ips | grep ${ENV_NAME}
-
-[ $RESULT -eq 0 ] && { echo "Tests succeeded"; exit $RESULT; }
+./plugin_test/utils/jenkins/system_tests.sh -t test ${systest_parameters} -i ${ISO_PATH} -j ${JOB_NAME} -o --group=${TEST_GROUP} 2>&1
