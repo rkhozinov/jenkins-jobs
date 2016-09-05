8837c7886c2f9e970a0081b52fddc2d1e1cec33b
naumvd95 <naumvd95@gmail.com>
2016-09-05 13:37:35 +0300

    unused code was removed, hardcode cases added instead of case-suites

---
+++ servers/partner-ci/builders/maintenance-update.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+set -ex
+
+###################### Get MIRROR HOST ###############
+
+LOCATION=${LOCATION_FACT:-bud}
+CENTOS_UPDATE_HOST="http://pkg-updates.fuel-infra.org/centos7/"
+MIRROR_HOST="http://mirror.seed-cz1.fuel-infra.org"
+
+###################### Get MIRROR_UBUNTU ###############
+
+if [[ ! "${MIRROR_UBUNTU}" ]]; then
+
+    UBUNTU_MIRROR_URL="$(curl ${MIRROR_HOST}/pkgs/ubuntu-latest.htm)"
+    UBUNTU_REPOS="deb ${UBUNTU_MIRROR_URL} trusty main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-updates main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-security main universe multiverse"
+
+    if [ "$ENABLE_PROPOSED" = true ]; then
+        UBUNTU_PROPOSED="deb ${UBUNTU_MIRROR_URL} trusty-proposed main universe multiverse"
+        UBUNTU_REPOS="$UBUNTU_REPOS|$UBUNTU_PROPOSED"
+    fi
+
+    export MIRROR_UBUNTU="$UBUNTU_REPOS"
+
+fi
+
+###################### Set extra DEB and RPM repos ####
+
+if [[ -n "${RPM_LATEST}" ]]; then
+    RPM_MIRROR="${MIRROR_HOST}mos-repos/centos/mos8.0-centos7-fuel/snapshots/"
+    if [[ "${ENABLE_PROPOSED}" == "true" ]]; then
+        RPM_PROPOSED="mos-proposed,${RPM_MIRROR}proposed-${RPM_LATEST}/x86_64"
+        EXTRA_RPM_REPOS+="${RPM_PROPOSED}"
+        UPDATE_FUEL_MIRROR="${RPM_MIRROR}proposed-${RPM_LATEST}/x86_64"
+    fi
+    if [[ "${ENABLE_UPDATES}" == "true" ]]; then
+        RPM_UPDATES="mos-updates,${RPM_MIRROR}updates-${RPM_LATEST}/x86_64"
+        if [[ -n "${EXTRA_RPM_REPOS}" ]]; then
+            EXTRA_RPM_REPOS+="|"
+            UPDATE_FUEL_MIRROR+=" "
+        fi
+        EXTRA_RPM_REPOS+="${RPM_UPDATES}"
+        UPDATE_FUEL_MIRROR+="${RPM_MIRROR}updates-${RPM_LATEST}/x86_64"
+    fi
+    if [[ "${ENABLE_SECURITY}" == "true" ]]; then
+        RPM_SECURITY="mos-security,${RPM_MIRROR}security-${RPM_LATEST}/x86_64"
+        if [[ -n "${EXTRA_RPM_REPOS}" ]]; then
+            EXTRA_RPM_REPOS+="|"
+            UPDATE_FUEL_MIRROR+=" "
+        fi
+        EXTRA_RPM_REPOS+="${RPM_SECURITY}"
+        UPDATE_FUEL_MIRROR+="${RPM_MIRROR}security-${RPM_LATEST}/x86_64"
+    fi
+    if [[ "${ENABLE_UPDATE_CENTOS}" == "true" ]]; then
+        RPM_UPDATE_CENTOS="centos-security,${CENTOS_UPDATE_HOST}"
+        if [[ -n "${EXTRA_RPM_REPOS}" ]]; then
+            EXTRA_RPM_REPOS+="|"
+            UPDATE_FUEL_MIRROR+=" "
+        fi
+        EXTRA_RPM_REPOS+="${RPM_UPDATE_CENTOS}"
+        UPDATE_FUEL_MIRROR+="${CENTOS_UPDATE_HOST}"
+    fi
+    export EXTRA_RPM_REPOS
+    export UPDATE_FUEL_MIRROR
+fi
+
+if [[ -n "${DEB_LATEST}" ]]; then
+    DEB_MIRROR="${MIRROR_HOST}mos-repos/ubuntu/snapshots/8.0-${DEB_LATEST}"
+    if [[ "${ENABLE_PROPOSED}" == "true" ]]; then
+        DEB_PROPOSED="mos-proposed,deb ${DEB_MIRROR} mos8.0-proposed main restricted"
+        EXTRA_DEB_REPOS+="${DEB_PROPOSED}"
+    fi
+    if [[ "${ENABLE_UPDATES}" == "true" ]]; then
+        DEB_UPDATES="mos-updates,deb ${DEB_MIRROR} mos8.0-updates main restricted"
+        if [[ -n "${EXTRA_DEB_REPOS}" ]]; then
+            EXTRA_DEB_REPOS+="|"
+        fi
+        EXTRA_DEB_REPOS+="${DEB_UPDATES}"
+    fi
+    if [[ "${ENABLE_SECURITY}" == "true" ]]; then
+        DEB_SECURITY="mos-security,deb ${DEB_MIRROR} mos8.0-security main restricted"
+        if [[ -n "${EXTRA_DEB_REPOS}" ]]; then
+            EXTRA_DEB_REPOS+="|"
+        fi
+        EXTRA_DEB_REPOS+="${DEB_SECURITY}"
+    fi
+    export EXTRA_DEB_REPOS
+fi
