4e9c42e06104ff3396af10dda4104e50bceb026f
naumvd95 <naumvd95@gmail.com>
2016-09-21 17:08:22 +0300

    https://mirantis.jira.com/browse/FPL-1856

--- servers/partner-ci/builders/nsxt.test.sh
+++ servers/partner-ci/builders/nsxt.test.sh
@@ -5,44 +5,36 @@
 export ISO_PATH="${ISO_STORAGE}/${ISO_FILE}"
 [ -z $ISO_PATH  ] && { echo "ISO_PATH is empty or doesn't exist"; exit 1; }
 
-if [[ $ISO_FILE == *"custom"* ]]; then
-  export FUEL_RELEASE=90
-else
-  export FUEL_RELEASE=$(echo $ISO_FILE | cut -d- -f2 | tr -d '.iso')
-fi
+export FUEL_RELEASE=$(echo $ISO_FILE | cut -d- -f2 | tr -d '.iso')
 
-if [ "${SNAPSHOTS_ID}" != "released" ]; then
-  if [[ "${UPDATE_MASTER}" == "true" ]] && [[ ${FUEL_RELEASE} != *"80"* ]]; then
-    if [ -f $SNAPSHOT_OUTPUT_FILE ]; then
-      . $SNAPSHOT_OUTPUT_FILE
-      export EXTRA_RPM_REPOS
-      export UPDATE_FUEL_MIRROR
-      export EXTRA_DEB_REPOS
-    else
-      echo "SNAPSHOT_OUTPUT_FILE is empty or doesn't exist"; exit 1
-    fi
+if [[ "${UPDATE_MASTER}" == "true" ]] && [[ ${FUEL_RELEASE} != *"80"* ]]; then
+  if [ -f $SNAPSHOT_OUTPUT_FILE ]; then
+    . $SNAPSHOT_OUTPUT_FILE
+    export EXTRA_RPM_REPOS
+    export UPDATE_FUEL_MIRROR
+    export EXTRA_DEB_REPOS
   else
-    export SNAPSHOTS_ID="released"
+    echo "SNAPSHOT_OUTPUT_FILE is empty or doesn't exist"
+    exit 1
   fi
+else
+  export SNAPSHOTS_ID="released"
 fi
 
-[[ $SNAPSHOTS_ID == *"lastSuccessfulBuild"* ]] && \
+if [[ $SNAPSHOTS_ID == *"lastSuccessfulBuild"* ]]; then
   export SNAPSHOTS_ID=$(cat snapshots.params | grep -Po '#\K[^ ]+')
+fi
 
 [ -z "${SNAPSHOTS_ID}" ] && { echo SNAPSHOTS_ID is empty; exit 1; }
 
 if [ -f build.plugin_version ]; then
   export PLUGIN_VERSION=$(grep "PLUGIN_VERSION" < build.plugin_version | cut -d= -f2 )
 else
-  if [ -z $PLUGIN_VERSION ]; then
-    echo "build.properties file is not available so a test couldn't be runned"
-    exit 1
-  fi  
+  echo "build.properties file is not available so a test couldn't be runned"
+  exit 1
 fi
 
-[ -z $PLUGIN_VERSION  ] && \
-  { echo "PLUGIN_VERSION variable is empty"; exit 1; } || \
-    export NSXT_PLUGIN_VERSION=$PLUGIN_VERSION
+[ -z $PLUGIN_VERSION  ] && { echo "PLUGIN_VERSION variable is empty"; exit 1; } || export NSXT_PLUGIN_VERSION=$PLUGIN_VERSION
 
 if [ -z "${PKG_JOB_BUILD_NUMBER}" ]; then
     if [ -f build.properties ]; then
@@ -68,7 +60,7 @@ export VENV_PATH="${HOME}/${FUEL_RELEASE}-venv"
 [ -z "${PLUGIN_PATH}"       ] && export PLUGIN_PATH=$NSXT_PLUGIN_PATH
 
 systest_parameters=''
-[[ $FORCE_REUSE == "true"  ]] && systest_parameters+=' -k' || echo "new env will be created"
+[[ $USE_SNAPSHOTS == "true"  ]] && systest_parameters+=' -k' || echo "new env will be created"
 [[ $ERASE_AFTER   == "true"  ]] && echo "the env will be erased after test" || systest_parameters+=' -K'
 
 echo "test-group: ${TEST_GROUP}"
@@ -99,6 +91,4 @@ REPORTER_PROPERTIES
 
 source "${VENV_PATH}/bin/activate"
 
-bash plugin_test/utils/jenkins/system_tests.sh -t test ${systest_parameters} -i ${ISO_PATH} -j ${JOB_NAME} -o --group=${TEST_GROUP} 2>&1
-sudo cp /var/log/libvirt/libvirtd.log ${WORKSPACE}/libvirtd_after_test.log
-sudo chown jenkins:jenkins ${WORKSPACE}/libvirtd_after_test.log
+./plugin_test/utils/jenkins/system_tests.sh -t test ${systest_parameters} -i ${ISO_PATH} -j ${JOB_NAME} -o --group=${TEST_GROUP} 2>&1
