9481f31694ba0a45b0a1a6e5f0388ab862390b05
naumvd95 <naumvd95@gmail.com>
2016-09-27 18:09:57 +0300

    9.0/contrail: refactoring

--- servers/partner-ci/builders/build-job-rst-testcases-upload.sh
+++ servers/partner-ci/builders/build-job-rst-testcases-upload.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -e
 export TESTRAIL_USER=$TREP_TESTRAIL_USER
 export TESTRAIL_API_KEY=$TREP_TESTRAIL_PASSWORD
-RST2TR_HOME="${WORKSPACE}/rst2tr"
-PATH_TO_DOCS="${WORKSPACE}/fuel-plugin-contrail/doc/testing/"
-cd $RST2TR_HOME
-if [ "${MODERN_MODEL}" == "true" ] && MODEL="$RST2TR_HOME/formats/modern_model.yaml" || MODEL="$RST2TR_HOME/formats/classic_model.yaml"
+PATH_TO_DOCS="${WORKSPACE}/contrail-repo/doc/testing/"
+[ "${MODERN_MODEL}" == "true" ] && MODEL="${WORKSPACE}/utility-repo/formats/modern_model.yaml" || MODEL="${WORKSPACE}/utility-repo/formats/classic_model.yaml"
 if [[ "${CHECK_ONLY}" == "true" ]]; then
-  python $RST2TR_HOME/rst2tr/rst2tr.py -f $MODEL -v -s -n ${PATH_TO_DOCS}
+  python ${WORKSPACE}/utility-repo/rst2tr/rst2tr.py -f $MODEL -v -s -n ${PATH_TO_DOCS}
 else
-  python $RST2TR_HOME/rst2tr/rst2tr.py -f $MODEL -v -s ${PATH_TO_DOCS}
+  python ${WORKSPACE}/utility-repo/rst2tr/rst2tr.py -f $MODEL -v -s ${PATH_TO_DOCS}
+fi
