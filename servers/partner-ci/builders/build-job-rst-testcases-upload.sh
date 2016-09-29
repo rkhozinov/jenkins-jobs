1eb40d03f3414685229fdd864ce8c7ad88693b2e
naumvd95 <naumvd95@gmail.com>
2016-09-29 12:42:06 +0300

    configuring tempest job

--- servers/partner-ci/builders/build-job-rst-testcases-upload.sh
+++ servers/partner-ci/builders/build-job-rst-testcases-upload.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -e
 export TESTRAIL_USER=$TREP_TESTRAIL_USER
 export TESTRAIL_API_KEY=$TREP_TESTRAIL_PASSWORD
-PATH_TO_DOCS="${WORKSPACE}/contrail-repo/doc/testing/"
-[ "${MODERN_MODEL}" == "true" ] && MODEL="${WORKSPACE}/utility-repo/formats/modern_model.yaml" || MODEL="${WORKSPACE}/utility-repo/formats/classic_model.yaml"
+PATH_TO_DOCS="${WORKSPACE}/doc/testing/"
+rm -rf ${WORKSPACE}/utility-repo && mkdir ${WORKSPACE}/utility-repo
+cd ${WORKSPACE}/utility-repo
+git clone https://github.com/ehles/rst2tr.git
+[ "${MODERN_MODEL}" == "true" ] && MODEL="${WORKSPACE}/utility-repo/rst2tr/formats/modern_model.yaml" || MODEL="${WORKSPACE}/utility-repo/rst2tr/formats/classic_model.yaml"
 if [[ "${CHECK_ONLY}" == "true" ]]; then
-  python ${WORKSPACE}/utility-repo/rst2tr/rst2tr.py -n -f $MODEL -v -s  ${PATH_TO_DOCS}
+  python ${WORKSPACE}/utility-repo/rst2tr/rst2tr/rst2tr.py -n -f $MODEL -v -s  ${PATH_TO_DOCS}
 else
-  python ${WORKSPACE}/utility-repo/rst2tr/rst2tr.py -f $MODEL -v -s ${PATH_TO_DOCS}
+  python ${WORKSPACE}/utility-repo/rst2tr/rst2tr/rst2tr.py -f $MODEL -v -s ${PATH_TO_DOCS}
 fi
