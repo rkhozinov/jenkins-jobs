0ac3f532f2ad571f60ba8b9b6d8961a9e3e32388
Ruslan Khozinov <ruslan.khozinov@gmail.com>
2016-12-20 11:14:28 +0000

    9.x upgrade tests are added

---
+++ servers/partner-ci/builders/upgrade.test.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -e
+# activate bash xtrace for script
+[[ "${DEBUG}" == "true" ]] && set -x || set +x
+
+export ISO_PATH=${ISO_PATH:-"$ISO_STORAGE/$ISO_FILE"}
+
+fuel_release=$(echo $ISO_FILE | cut -d- -f2 | tr -d '.iso')
+export FUEL_RELEASE=$fuel_release
+
+if [ "${SNAPSHOTS_ID}" != "released" ]; then
+  if [[ "${UPDATE_MASTER}" == "true" ]] && [[ ${FUEL_RELEASE} != *"80"* ]]; then
+    if [ -f $SNAPSHOT_OUTPUT_FILE ]; then
+      . $SNAPSHOT_OUTPUT_FILE
+      export EXTRA_RPM_REPOS
+      export UPDATE_FUEL_MIRROR
+      export EXTRA_DEB_REPOS
+    else
+      echo "SNAPSHOT_OUTPUT_FILE is empty or doesn't exist"
+      exit 1
+    fi
+  else
+    export SNAPSHOTS_ID="released"
+  fi
+fi
+
+[[ $SNAPSHOTS_ID == *"lastSuccessfulBuild"* ]] && \
+  export SNAPSHOTS_ID=$(grep -Po '#\K[^ ]+' < snapshots.params)
+
+if [ "${SNAPSHOTS_ID}" != "released" ]; then
+  version=$(grep "PLUGIN_VERSION" < build.plugin_version | cut -d= -f2 )
+  export PLUGIN_VERSION=${version:?}
+fi
+
+#remove old logs and test data
+[ -f nosetest.xml ] && sudo rm -f nosetests.xml
+sudo rm -rf logs/*
+
+export ENV_NAME="${ENV_PREFIX:?}.${SNAPSHOTS_ID:?}"
+export VENV_PATH="${HOME}/${FUEL_RELEASE:?}-venv"
+
+
+. "${VENV_PATH}/bin/activate"
+
+sh -x "utils/jenkins/system_tests.sh" \
+   -k                                 \
+   -K                                 \
+   - w "{$WORKSPACE}"                 \
+   -t test                            \
+   -o                                 \
+   --group="${TEST_GROUP}"            \
+   -i ${ISO_PATH:?}                   \
+
+echo "ENVIRONMENT NAME is $ENV_NAME"
+dos.py list --ips | grep ${ENV_NAME}
+
