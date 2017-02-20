b4ad4bb31a872dc7370331df5162251b49513e2d
Ruslan Khozinov <ruslan.khozinov@gmail.com>
2017-02-20 15:23:09 +0000

    10.0/emc: builder should use job's PUPPETLINT_PATH

--- servers/partner-ci/builders/build-plugin-emc.sh
+++ servers/partner-ci/builders/build-plugin-emc.sh
@@ -3,11 +3,9 @@ set -e
 
 git log  --pretty=oneline | head
 
-path="./deployment_scripts/puppet/manifests ./deployment_scripts/puppet/modules/plugin_emc_vnx"
-
-find $path -name '*.erb' -print0 | xargs -0 -P1 -L1 -I '%' erb -P -x -T '-' % | ruby -c
-find $path -name '*.pp' -print0  | xargs -0 -P1 -L1 puppet parser validate --verbose
-find $path -name '*.pp' -print0  | xargs -0 -P1 -L1 -r puppet-lint \
+find $PUPPETLINT_PATH -name '*.erb' -print0 | xargs -0 -P1 -L1 -I '%' erb -P -x -T '-' % | ruby -c
+find $PUPPETLINT_PATH -name '*.pp' -print0  | xargs -0 -P1 -L1 puppet parser validate --verbose
+find $PUPPETLINT_PATH -name '*.pp' -print0  | xargs -0 -P1 -L1 -r puppet-lint \
          --fail-on-warnings \
          --with-context \
          --with-filename \
