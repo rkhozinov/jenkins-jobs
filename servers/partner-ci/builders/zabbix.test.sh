c6d4b566a184a20ce991115f7befd16a2ff58840
naumvd95 <naumvd95@gmail.com>
2017-02-27 16:05:35 +0300

    10.0/zabbix: remove build numbers from test script

--- servers/partner-ci/builders/zabbix.test.sh
+++ servers/partner-ci/builders/zabbix.test.sh
@@ -2,19 +2,6 @@
 # activate bash xtrace for script
 [[ "${DEBUG}" == "true" ]] && set -x || set +x
 
-# Export build numbers
-zabbix_external_build_version=$(grep "BUILD_NUMBER" < fuel-plugin-external-zabbix.properties | cut -d= -f2 )
-export ZABBIX_EXTERNAL_PKG_JOB_BUILD_NUMBER=${PKG_JOB_BUILD_NUMBER:-$zabbix_external_build_version}
-
-zabbix_emc_build_version=$(grep "BUILD_NUMBER" < fuel-plugin-zabbix-monitoring-emc.properties | cut -d= -f2 )
-export ZABBIX_EMC_PKG_JOB_BUILD_NUMBER=${PKG_JOB_BUILD_NUMBER:-$zabbix_emc_build_version}
-
-zabbix_extr_net_build_version=$(grep "BUILD_NUMBER" < fuel-plugin-zabbix-monitoring-extreme-networks.properties | cut -d= -f2 )
-export ZABBIX_EXTR_NET_PKG_JOB_BUILD_NUMBER=${PKG_JOB_BUILD_NUMBER:-$zabbix_extr_net_build_version}
-
-zabbix_snmp_build_version=$(grep "BUILD_NUMBER" < fuel-plugin-zabbix-snmptrapd.properties | cut -d= -f2 )
-export ZABBIX_SNMP_PKG_JOB_BUILD_NUMBER=${PKG_JOB_BUILD_NUMBER:-$zabbix_snmp_build_version}
-
 # Export paths for plugins
 zabbix_plugin_path=$(ls -t ${WORKSPACE}/zabbix_monitoring-*.rpm | head -n 1)
 export ZABBIX_MONITORING_PLUGIN_PATH=${ZABBIX_MONITORING_PLUGIN_PATH:-$zabbix_plugin_path}
@@ -35,10 +22,10 @@ echo -e "test-group: ${TEST_GROUP}\n" \
         "venv-path: ${VENV_PATH}\n" \
         "env-name: ${ENV_NAME}\n" \
         "iso-path: ${ISO_PATH}\n" \
-        "zabbix-external-plugin-path: ${ZABBIX_EXTERNAL_PLUGIN_PATH:?}\n" \
-        "zabbix-emc-plugin-path: ${ZABBIX_EMC_PLUGIN_PATH:?}\n" \
-        "zabbix-extr-net-plugin-path: ${ZABBIX_EXTR_NET_PLUGIN_PATH:?}\n" \
-        "zabbix-snmp-plugin-path: ${ZABBIX_SNMP_PLUGIN_PATH:?}\n"
+        "zabbix-external-plugin-path: ${ZABBIX_MONITORING_PLUGIN_PATH:?}\n" \
+        "zabbix-emc-plugin-path: ${ZABBIX_MONITORING_EMC_PLUGIN_PATH:?}\n" \
+        "zabbix-extr-net-plugin-path: ${ZABBIX_MONITORING_EXTREME_NETWORKS_PLUGIN_PATH:?}\n" \
+        "zabbix-snmp-plugin-path: ${ZABBIX_MONITORING_SNMPTRAPD_PLUGIN_PATH:?}\n"
 
 . "${VENV_PATH}/bin/activate"
 
