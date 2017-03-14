f04f3ab8b8ec235856ea0d7772c25bda743122cf
naumvd95 <naumvd95@gmail.com>
2017-03-14 16:47:17 +0300

    10.0/fake-ui: get rid of excess common.prepare

--- servers/partner-ci/builders/ui.fake.test.sh
+++ servers/partner-ci/builders/ui.fake.test.sh
@@ -21,7 +21,7 @@ export TEST_PATH="${TEST_PREFIX:?}/${TEST_GROUP:?}.js"
 
 . "${VENV_PATH}/bin/activate"
 cd ${WORKSPACE}/docker/
-pip install --upgrade docker-compose
+
 ln -s ${WORKSPACE}/docker/fuel-web fuel-web
 docker-compose down -v
 docker-compose up --remove-orphans --build --abort-on-container-exit
